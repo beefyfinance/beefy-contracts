5b4431665cbe3d63fc267bcd42b1ec8aed3f363a
shibacrypt <>
2021-01-10 13:01:48 +0900

    Add bdollar strategy

---
+++ contracts/BIFI/interfaces/bdollar/IBdoRewardPool.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IBdoRewardPool {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function pendingBDO(uint256 _pid, address _user) external view returns (uint256);
+    function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
+    function emergencyWithdraw(uint256 _pid) external;
+}
