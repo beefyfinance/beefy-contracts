718848629d804c1d0fb4e7e65bc05084f9860c2d
sirbeefalot <sirbeefalot@pm.me>
2021-03-05 18:18:58 -0300

    unpause auto bnb

--- scripts/deploy-launchpad-pool.js
+++ scripts/deploy-launchpad-pool.js
@@ -4,13 +4,15 @@ const registerSubsidy = require("../utils/registerSubsidy");
 
 const ethers = hardhat.ethers;
 
+const abi = ["function notifyRewards() external"];
+
 const pools = [
   {
-    name: "TWT Pool",
-    stakedToken: "0x4B0F1812e5Df2A09796481Ff14017e6005508003",
-    rewardsToken: "",
-    durationInSec: 604800,
-    capPerAddr: "1000000000000000000000",
+    name: "SALT Pool",
+    stakedToken: "0xe0B473c0dD6D7Fea5B395c3Ce7ffd4FEF0ab4373",
+    rewardsToken: "0x2849b1aE7E04A3D9Bc288673A92477CF63F28aF4",
+    durationInSec: 432000,
+    capPerAddr: "1000000000000000000000000000000",
   },
 ];
 
@@ -21,6 +23,8 @@ async function main() {
 
   for (pool of pools) {
     console.log("Deploying:", pool.name);
+
+    const [deployer] = await ethers.getSigners();
 
     const launchpad = await Launchpad.deploy(pool.stakedToken, pool.rewardsToken, pool.durationInSec, pool.capPerAddr);
     await launchpad.deployed();
