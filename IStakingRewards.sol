373cdac122c136ff1442555eb5f00d9706c7dfb5
MirthFutures <admin@betswapexchange.com>
2021-03-12 08:30:44 -0500

    Add Pangolin Strat

---
+++ IStakingRewards.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.0;
+
+interface IStakingRewards {
+    function stake(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function getReward() external;
+    function balanceOf(address account) external view returns (uint256);
+}
