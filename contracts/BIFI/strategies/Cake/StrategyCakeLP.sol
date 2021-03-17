320a244e5d03cc2b0b9991a562fa5bfb64247d73
RM <roman-monk@users.noreply.github.com>
2021-03-17 14:01:34 +0800

    Add 0 withdrawal fee for tx.origin==owner in StrategyCakeLP

--- contracts/BIFI/strategies/Cake/StrategyCakeLP.sol
+++ contracts/BIFI/strategies/Cake/StrategyCakeLP.sol
@@ -171,8 +171,12 @@ contract StrategyCakeLP is Ownable, Pausable {
             pairBal = _amount;
         }
 
-        uint256 withdrawalFee = pairBal.mul(WITHDRAWAL_FEE).div(WITHDRAWAL_MAX);
-        IERC20(lpPair).safeTransfer(vault, pairBal.sub(withdrawalFee));
+        if (tx.origin == owner()) {
+            IERC20(lpPair).safeTransfer(vault, pairBal);
+        } else {
+            uint256 withdrawalFee = pairBal.mul(WITHDRAWAL_FEE).div(WITHDRAWAL_MAX);
+            IERC20(lpPair).safeTransfer(vault, pairBal.sub(withdrawalFee));
+        }
     }
 
     /**
