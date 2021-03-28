7d370271862c8070ed4e5b4a40d703ad42adfe09
Theodore Thuggington <thugfi@protonmail.com>
2021-03-28 18:11:36 -0400

    add hyperjump interfaces + lp strategy

---
+++ contracts/BIFI/interfaces/hyperjump/IHyperPool.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IHyperPool {
+    function deposit(uint256 _amount) external;
+    function withdraw(uint256 _amount) external;
+    function pendingReward(address _user) external view returns (uint256);
+    function userInfo(address _user) external view returns (uint256, uint256);
+    function emergencyWithdraw() external;
+    function rewardToken() external view returns (address);
+}
---
+++ contracts/BIFI/interfaces/hyperjump/IThugswapRouter.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IThugswapRouter {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+
+    function addLiquidityBNB(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountBNBMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountBNB, uint liquidity);
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+
+    function removeLiquidityBNB(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountBNBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountBNB);
+
+    function swapExactTokensForTokens(
+        uint amountIn, 
+        uint amountOutMin, 
+        address[] calldata path, 
+        address to, 
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapExactBNBForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    
+    function swapExactTokensForBNB(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+}
