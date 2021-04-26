4cbea2f81e123816f5c90830d26c1544030d71c3
MirthFutures <admin@betswapexchange.com>
2021-04-26 11:39:48 -0400

    Add Yield Optimizer Strat

---
+++ contracts/BIFI/interfaces/common/IFarm.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+interface IFarm {
+    function poolInfo(uint256 _pid) external view returns (address, uint256, uint256, uint256, address);
+    function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
+    function stakedWantTokens(uint256 _pid, address _user) external view returns (uint256);
+    function deposit(uint256 _pid, uint256 _wantAmt) external;
+    function withdraw(uint256 _pid, uint256 _wantAmt) external;
+    function withdrawAll(uint256 _pid) external;
+    function emergencyWithdraw(uint256 _pid) external;
+}
