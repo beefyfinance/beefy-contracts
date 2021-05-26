ce2cfad110ad4e26343eb8550d0002fbfbfe9a1c
sirbeefalot <sirbeefalot@pm.me>
2021-05-26 15:55:25 -0300

    Only register subsidy on BSC

--- utils/registerSubsidy.js
+++ utils/registerSubsidy.js
@@ -2,6 +2,8 @@ const ethers = require("ethers");
 const IRewardRegister = require("../artifacts/contracts/BIFI/interfaces/binance/IRewardRegister.sol/IRewardRegister.json");
 
 const registerSubsidy = async (contract, deployer) => {
+  if (deployer.provider._hardhatProvider._provider._chainId !== 56) return;
+
   const register = "0xCad9146102D29175Fd7908EB6820A48E4FC78CEA";
   const registerContract = new ethers.Contract(register, IRewardRegister.abi, deployer);
 
