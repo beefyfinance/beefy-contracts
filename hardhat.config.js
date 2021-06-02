e737036fa56d2520e9e4f4b9b2d0c25938559f00
Nathan Loewen <loewen.nathan@gmail.com>
2021-06-02 01:38:47 -0500

    Get Hardhat to help out with accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
 require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+
+const DEPLOYER_PK_FILE = ".config/DEPLOYER_PK";
+const OTHER_PK_FILE    = ".config/OTHER_PK";
 
 task("panic", "Panics a given strategy.")
   .addParam("strat", "The strategy to panic.")
@@ -47,9 +51,75 @@ task("harvest", "Harvests a given strategy.")
     }
   });
 
+task("generate_accounts", "Creates new deployer and test accounts")
+  .setAction(async _ => {
+    let account;
+    let file;
+
+    try {
+      fs.mkdirSync(".config");
+    }
+    catch (e) {}
+
+    try {
+      file = fs.openSync(DEPLOYER_PK_FILE, 'wx+', 0o600);
+      account = ethers.Wallet.createRandom();
+      fs.writeFileSync(file, account.privateKey);
+      console.log(`Deployer account: ${account.address}`);
+    }
+    catch (e) {
+      if (e.code === 'EEXIST') {
+        console.log("Deployer key exists. Not overwriting");
+      }
+      else {
+        console.error(e);
+      }
+    }
+    finally {
+      if (file) {
+        fs.closeSync(file);
+        file = null;
+      }
+    }
+
+    try {
+      file = fs.openSync(OTHER_PK_FILE, 'wx+', 0o600);
+      account = ethers.Wallet.createRandom().privateKey;
+      fs.writeFileSync(file, account);
+      console.log(`Other account  : ${account.address}`);
+    }
+    catch (e) {
+      if (e.code === 'EEXIST') {
+        console.log("Other key exists. Not overwriting");
+      }
+      else {
+        console.error(e);
+      }
+    }
+    finally {
+      if (file) {
+        fs.closeSync(file);
+        file = null;
+      }
+    }
+  });
+
 let deployerAccount;
 if (process.env.DEPLOYER_PK)
   deployerAccount = [process.env.DEPLOYER_PK];
+else {
+  try {
+    deployerAccount = [fs.readFileSync(DEPLOYER_PK_FILE).toString()];
+  }
+  catch (e) {
+    if (e.code === 'ENOENT') {
+      console.log("Deployer account not found. Create .config/DEPLOYER_PK or run `hardhat generate_accounts`.");
+    }
+    else {
+      console.error(e);
+    }
+  }
+}
 
 module.exports = {
   defaultNetwork: "localhost",
