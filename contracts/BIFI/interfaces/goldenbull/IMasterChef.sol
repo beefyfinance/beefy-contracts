b3c74f576e4f021bf70aa431c23f82c75b985ab6
Moonster <moonster.bsc@gmail.com>
2021-06-08 17:27:47 -0400

    semi

--- contracts/BIFI/interfaces/goldenbull/IMasterChef.sol
+++ contracts/BIFI/interfaces/goldenbull/IMasterChef.sol
@@ -8,5 +8,5 @@ interface IMasterChef {
     function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
     function emergencyWithdraw(uint256 _pid) external;
     function deposit(uint256 _pid, uint256 _amount, address _referrer) external;
-    function canHarvest(uint256 _pid, address _user) external view returns (bool)
+    function canHarvest(uint256 _pid, address _user) external view returns (bool);
 }
