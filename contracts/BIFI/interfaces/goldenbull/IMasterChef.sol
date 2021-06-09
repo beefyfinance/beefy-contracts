7f18db2c088315df542d21bce5a488d016fc79bf
Moonster-BSC <83176690+Moonster-BSC@users.noreply.github.com>
2021-06-08 20:31:18 -0400

    Goldenbull finance Vault + Strat (#62)
    vault + strat

---
+++ contracts/BIFI/interfaces/goldenbull/IMasterChef.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IMasterChef {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
+    function emergencyWithdraw(uint256 _pid) external;
+    function deposit(uint256 _pid, uint256 _amount, address _referrer) external;
+    function canHarvest(uint256 _pid, address _user) external view returns (bool);
+}
