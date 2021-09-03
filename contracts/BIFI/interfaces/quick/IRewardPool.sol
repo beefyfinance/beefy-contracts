bd24dd128cde080db7e6e4ac32f87b1d55cdd304
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-09-03 12:10:40 +0100

    add quick single

---
+++ contracts/BIFI/interfaces/quick/IRewardPool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IRewardPool {
+    function deposit(uint256 amount) external;
+    function stake(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function getReward() external;
+    function balanceOf(address account) external view returns (uint256);
+    function stakingToken() external view returns (address);
+    function rewardsToken() external view returns (address);
+}
