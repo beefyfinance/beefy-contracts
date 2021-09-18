af00d7cd2188f0fb7c8e46410d604f36d3d3fb4e
Moonster <moonster.bsc@gmail.com>
2021-09-18 00:33:31 -0400

    Setup for hardhat native forking

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,54 +2,58 @@ import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
-import './tasks';
+import "./tasks";
 
 import { HardhatUserConfig } from "hardhat/src/types/config";
 import { HardhatUserConfig as WithEtherscanConfig } from "hardhat/config";
+import { buildHardhatNetworkAccounts, getPKs } from "./utils/configInit";
 
 type DeploymentConfig = HardhatUserConfig & WithEtherscanConfig;
 
-let deployerAccount;
-if (process.env.DEPLOYER_PK) deployerAccount = [process.env.DEPLOYER_PK];
+const accounts = getPKs();
+const hardhatNetworkAccounts = buildHardhatNetworkAccounts(accounts);
 
 const config: DeploymentConfig = {
   defaultNetwork: "localhost",
   networks: {
-    hardhat: {},
+    hardhat: {
+      // accounts visible to hardhat network used by `hardhat node --fork` (yarn net <chainName>)
+      accounts: hardhatNetworkAccounts,
+    },
     bsc: {
       url: "https://bsc-dataseed2.defibit.io/",
       chainId: 56,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     heco: {
       url: "https://http-mainnet-node.huobichain.com",
       chainId: 128,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     avax: {
       url: "https://api.avax.network/ext/bc/C/rpc",
       chainId: 43114,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     polygon: {
       url: "https://polygon-rpc.com/",
       chainId: 137,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     fantom: {
       url: "https://rpc.ftm.tools",
       chainId: 250,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     one: {
       url: "https://api.s0.t.hmny.io/",
       chainId: 1666600000,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     arbitrum: {
       url: "https://arb1.arbitrum.io/rpc",
       chainId: 42161,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     localhost: {
       url: "http://127.0.0.1:8545",
@@ -59,13 +63,13 @@ const config: DeploymentConfig = {
     testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       chainId: 97,
-      accounts: deployerAccount,
+      accounts: accounts,
     },
     kovan: {
       url: "https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       chainId: 42,
-      accounts: deployerAccount,
-    }
+      accounts: accounts,
+    },
   },
   etherscan: {
     // Your API key for Etherscan
@@ -108,4 +112,4 @@ const config: DeploymentConfig = {
   },
 };
 
-export default config
+export default config;
