b378e3899d0f57c646a760d510add4765e615c28
Moonster <moonster.bsc@gmail.com>
2021-09-20 14:34:47 -0400

    add modifier

--- contracts/BIFI/utils/GasThrottlerArb.sol
+++ contracts/BIFI/utils/GasThrottlerArb.sol
@@ -4,40 +4,12 @@ pragma solidity ^0.6.12;
 
 import "./IGasPrice.sol";
 
-contract GasThrottlerArb {
+contract GasThrottler {
 
-    address public gasprice = address(0);
-    address public keeper;
+    address public gasprice = address(0xA43509661141F254F54D9A326E8Ec851A0b95307);
 
-    /**
-     * @param _keeper address that manages gas throttler.
-     */
-    constructor(
-        address _keeper
-    ) public {
-        keeper = _keeper;
-    }
-
-    modifier gasThrottle() {
-        require(gasprice != address(0) && tx.gasprice <= IGasPrice(gasprice).maxGasPrice(), "gas is too high!");
+    modifier gasThrottle(bool shouldGasThrottle) {
+        require(shouldGasThrottle && tx.gasprice <= IGasPrice(gasprice).maxGasPrice(), "gas is too high!");
         _;
-    }
-
-    // checks that caller is keeper.
-    modifier onlyKeeper() {
-        require(msg.sender == keeper, "!keeper");
-        _;
-    }
-
-    /**
-     * @dev Updates address of the keeper.
-     * @param _keeper new keeper address.
-     */
-    function setKeeper(address _keeper) external onlyKeeper {
-        keeper = _keeper;
-    }
-
-    function setGasprice(address _gasprice) external onlyKeeper {
-        gasprice = _gasprice;
     }
 }
