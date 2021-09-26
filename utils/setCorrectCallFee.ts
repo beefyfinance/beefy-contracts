960111ccfbbca1bba2ab9620b13cacffb9fbabbe
Moonster <moonster.bsc@gmail.com>
2021-09-26 12:33:13 -0400

    add quotes for clarity

--- utils/setCorrectCallFee.ts
+++ utils/setCorrectCallFee.ts
@@ -5,7 +5,7 @@ export const setCorrectCallFee = async (strategy: Contract, chainName: string) =
   const expectedCallFee = chainCallFeeMap[chainName];
   const defaultCallFee = await strategy.callFee();
   if (expectedCallFee !== defaultCallFee) {
-    console.log(`Setting call fee to ${expectedCallFee}`)
+    console.log(`Setting call fee to '${expectedCallFee}'`)
     await strategy.setCallFee(expectedCallFee);
   }
 }
