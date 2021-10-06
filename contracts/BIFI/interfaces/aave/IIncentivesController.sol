b1cb776e26e35b8483ee3ba34bf389562bc97099
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-10-06 16:07:58 +0100

    Update Aave strats

--- contracts/BIFI/interfaces/aave/IIncentivesController.sol
+++ contracts/BIFI/interfaces/aave/IIncentivesController.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.6.0;
 
 interface IIncentivesController {
     function claimRewards(address[] calldata assets, uint256 amount, address to) external returns (uint256);
+    function getRewardsBalance(address[] calldata assets, address user) external view returns (uint256);
 }
