b2f7bbcf799c1ec74492b3d27db367eee2f1ef04
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-11-06 18:29:20 +0000

    add geist lending strat

---
+++ contracts/BIFI/interfaces/geist/IMultiFeeDistributer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IMultiFeeDistributer {
+    struct RewardData {
+        address token;
+        uint256 amount;
+    }
+
+    function claimableRewards(address account) external view returns (RewardData[] memory rewards);
+    function exit() external;
+}
