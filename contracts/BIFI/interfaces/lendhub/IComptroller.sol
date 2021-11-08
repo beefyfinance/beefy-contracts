e0e50aa6f9b418fd98532189406ac569a5afb354
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-11-08 17:57:44 +0000

    add banker base strat

--- contracts/BIFI/interfaces/lendhub/IComptroller.sol
+++ contracts/BIFI/interfaces/lendhub/IComptroller.sol
@@ -8,4 +8,6 @@ interface IComptroller {
     function compAccrued(address holder) view external returns (uint256 comp);
     function enterMarkets(address[] memory _iTokens) external;
     function pendingComptrollerImplementation() view external returns (address implementation);
+    function pendingImplementation() view external returns (address implementation);
+    function claimReward(uint8 rewardType, address holder) external;
 }
