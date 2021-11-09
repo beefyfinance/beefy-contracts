8024c7738c61340191c5b8126fca3ad6f1dfaf38
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-11-09 09:23:11 +0000

    fix geist

--- contracts/BIFI/strategies/Geist/StrategyGeistNative.sol
+++ contracts/BIFI/strategies/Geist/StrategyGeistNative.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
