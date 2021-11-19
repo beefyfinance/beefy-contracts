8c7afd235df6b26dac8de88cfe1af5699818afb1
sirb <sirbeefalot@pm.me>
2021-11-19 16:44:14 -0300

    Remove unused import.

--- utils/verifyContract.ts
+++ utils/verifyContract.ts
@@ -1,5 +1,4 @@
 import hardhat from "hardhat";
-import { Contract } from "@ethersproject/contracts";
 
 export const verifyContract = async (address: string, constructorArguments: any[]) => {
   await hardhat.run("verify:verify", {
