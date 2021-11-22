27a6e1c3a4f91313e60d5791ab72cbbc573fd68a
RM <roman-monk@users.noreply.github.com>
2021-11-22 20:23:28 +0800

    StrategyBlizz

--- contracts/BIFI/strategies/Blizz/StrategyBlizzChefLP.sol
+++ contracts/BIFI/strategies/Blizz/StrategyBlizzChefLP.sol
@@ -10,19 +10,7 @@ import "../../interfaces/common/IUniswapRouterETH.sol";
 import "../../interfaces/common/IUniswapV2Pair.sol";
 import "../Common/StratManager.sol";
 import "../Common/FeeManager.sol";
-
-interface IBlizzMasterChef {
-    function deposit(address _token, uint256 _amount) external;
-    function withdraw(address _token, uint256 _amount) external;
-    function claim(address _user, address[] calldata _tokens) external;
-    function emergencyWithdraw(address _token) external;
-    function userInfo(address _token, address _user) external view returns (uint256, uint256);
-    function claimableReward(address _user, address[] calldata _tokens) external view returns (uint256[] memory);
-}
-
-interface IBlizzMultiFeeDistribution {
-    function exit(bool _claim) external;
-}
+import "./IBlizz.sol";
 
 contract StrategyBlizzChefLP is StratManager, FeeManager {
     using SafeERC20 for IERC20;
