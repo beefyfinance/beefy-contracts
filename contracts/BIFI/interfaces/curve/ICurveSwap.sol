83d2af022e54b6a455a76209d95dd9815d260d54
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-12-13 19:41:12 +0000

    Add Jarvis 4eur Strategy

--- contracts/BIFI/interfaces/curve/ICurveSwap.sol
+++ contracts/BIFI/interfaces/curve/ICurveSwap.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.6.0;
 
+interface ICurveSwap {
+    function remove_liquidity_one_coin(uint256 token_amount, int128 i, uint256 min_amount) external;
+    function calc_withdraw_one_coin(uint256 tokenAmount, uint256 i) external view returns (uint256);
+}
+
 interface ICurveSwap2 {
     function add_liquidity(uint256[2] memory amounts, uint256 min_mint_amount) external;
     function add_liquidity(uint256[2] memory amounts, uint256 min_mint_amount, bool _use_underlying) external;
