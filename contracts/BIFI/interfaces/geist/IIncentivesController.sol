7ee63c7a31432fc614260f95c57c91e6900eef28
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-12-16 20:48:31 +0000

    Fix Geist

--- contracts/BIFI/interfaces/geist/IIncentivesController.sol
+++ contracts/BIFI/interfaces/geist/IIncentivesController.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.6.0;
 
 interface IIncentivesController {
     function claimableReward(address _user, address[] calldata _tokens) external view returns (uint256[] memory);
+    function claim(address _user, address[] calldata _tokens) external;
 }
