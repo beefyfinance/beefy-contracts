35d834bf86cfdfd3f7a7251f8f1aeaa7c0a4678d
Moonster <Moonster.bsc@gmail.com>
2021-12-25 12:50:08 -0500

    polygon gas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import "./tasks";
 import { HardhatUserConfig } from "hardhat/src/types/config";
 import { HardhatUserConfig as WithEtherscanConfig } from "hardhat/config";
 import { buildHardhatNetworkAccounts, getPKs } from "./utils/configInit";
+import { ethers } from "ethers";
 
 type DeploymentConfig = HardhatUserConfig & WithEtherscanConfig;
 
@@ -41,6 +42,7 @@ const config: DeploymentConfig = {
       url: "https://polygon-rpc.com/",
       chainId: 137,
       accounts,
+      gasPrice: ethers.utils.parseUnits("100", "gwei").toNumber()
     },
     fantom: {
       url: "https://rpc.ftm.tools",
