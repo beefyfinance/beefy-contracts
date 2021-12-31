9b2e05ffee30fe0263dc7b23de02a5d945d14836
Moonster <Moonster.bsc@gmail.com>
2021-12-31 13:50:51 -0500

    init VaultGasOverheadAnalyzer

---
+++ contracts/BIFI/keepers/contracts/VaultGasOverheadAnalyzer.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
+
+import "./ManageableUpgradable.sol";
+
+import "../interfaces/IBeefyVault.sol";
+import "../interfaces/IBeefyStrategy.sol";
+
+contract VaultGasOverheadAnalyzer is ManageableUpgradable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
+
+    function initialize() public initializer {
+        __Manageable_init();
+    }
+
+    /**
+     * @dev Rescues random funds stuck.
+     * @param token_ address of the token to rescue.
+     */
+    function inCaseTokensGetStuck(address token_) external onlyManager {
+        IERC20Upgradeable token = IERC20Upgradeable(token_);
+
+        uint256 amount = token.balanceOf(address(this));
+        token.safeTransfer(msg.sender, amount);
+    }
+}
