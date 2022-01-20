8db55a7bf5cd2c1a5fbd74eb392e1f8ba361a592
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2022-01-20 10:43:47 +0000

    Add Jarvis 2CAD Strategy

--- contracts/BIFI/interfaces/curve/ICurveSwap.sol
+++ contracts/BIFI/interfaces/curve/ICurveSwap.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.6.0;
 interface ICurveSwap {
     function remove_liquidity_one_coin(uint256 token_amount, int128 i, uint256 min_amount) external;
     function calc_withdraw_one_coin(uint256 tokenAmount, int128 i) external view returns (uint256);
+    function coins(uint256 arg0) external view returns (address);
 }
 
 interface ICurveSwap2 {
