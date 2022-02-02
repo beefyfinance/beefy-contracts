29d1a26ac9b1d82ee9696cada29de6d56c637099
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2022-02-02 17:48:21 +0000

    Update StrategyCommonMultiRewardPoolLP

---
+++ contracts/BIFI/interfaces/common/IMultiRewardPool.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IMultiRewardPool {
+    function deposit(uint256 amount) external;
+    function stake(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function earned(address account) external view returns (uint256[] memory);
+    function getReward() external;
+    function balanceOf(address account) external view returns (uint256);
+}
