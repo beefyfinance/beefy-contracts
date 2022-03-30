79573fcf3eedc15745f35400576cfd4372cd7999
Commander Keen <0xCmdrKeen@pm.me>
2022-03-30 14:57:34 -0700

    Strategy for Hector TOR Curve LP

---
+++ contracts/BIFI/interfaces/hector/IStakingRewards.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IStakingRewards {
+    function stake(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function getReward() external;
+    function balanceOf(address account) external view returns (uint256);
+    function earned(address account) external view returns (uint256);
+}
