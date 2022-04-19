d19a1bac370167d28e138060625c1e90cdf9057e
MirthFutures <69535002+MirthFutures@users.noreply.github.com>
2022-04-19 10:56:46 -0400

    Merge (#185)
    * Start veJoe
    
    * Add Chef Manager and updates
    
    * A Few Fixes
    
    * verify
    
    * Add support for second reward
    
    * Add Joe tax to beJoe
    
    * fetch chef from strat
    
    * beJoe Immutable & deploy
    
    * Add Rose and Valas
    
    * update strat to new crv interface

---
+++ contracts/BIFI/strategies/Blizz/StrategyValas.sol
@@ -0,0 +1,429 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
+import "../../interfaces/aave/IDataProvider.sol";
+import "../../interfaces/aave/ILendingPool.sol";
+import "../../interfaces/common/IUniswapRouter.sol";
+import "../Common/FeeManager.sol";
+import "../Common/StratManager.sol";
+import "./IBlizz.sol";
+
+contract StrategyValas is StratManager, FeeManager {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    // Tokens used
+    address public want;
+    address public output;
+    address public native;
+
+    struct TokenAddresses {
+        address token;
+        address gToken;
+        address vToken;
+    }
+
+    TokenAddresses public wantTokens;
+
+    // Third party contracts
+    address public dataProvider = address(0xb5f344F568b714c471B23924644B4e393Ca4E9E9);
+    address public lendingPool = address(0xE29A55A6AEFf5C8B1beedE5bCF2F0Cb3AF8F91f5);
+    IBlizzMultiFeeDistribution public feeDistribution = IBlizzMultiFeeDistribution(0x685D3b02b9b0F044A3C01Dbb95408FC2eB15a3b3);
+    IBlizzIncentivesController public incentivesController = IBlizzIncentivesController(0xB7c1d99069a4eb582Fc04E7e1124794000e7ecBF);
+
+    // Routes
+    address[] public outputToNativeRoute;
+    address[] public outputToWantRoute;
+
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
+    /**
+     * @dev Variables that can be changed to config profitability and risk:
+     * {borrowRate}          - What % of our collateral do we borrow per leverage level.
+     * {borrowRateMax}       - A limit on how much we can push borrow risk.
+     * {borrowDepth}         - How many levels of leverage do we take.
+     * {minLeverage}         - The minimum amount of collateral required to leverage.
+     * {BORROW_DEPTH_MAX}    - A limit on how many steps we can leverage.
+     * {INTEREST_RATE_MODE}  - The type of borrow debt. Stable: 1, Variable: 2.
+     */
+    uint256 public borrowRate;
+    uint256 public borrowRateMax;
+    uint256 public borrowDepth;
+    uint256 public minLeverage;
+    uint256 constant public BORROW_DEPTH_MAX = 10;
+    uint256 constant public INTEREST_RATE_MODE = 2;
+
+    /**
+     * @dev Helps to differentiate borrowed funds that shouldn't be used in functions like 'deposit()'
+     * as they're required to deleverage correctly.
+     */
+    uint256 public reserves = 0;
+
+    /**
+     * @dev Events that the contract emits
+     */
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event StratRebalance(uint256 _borrowRate, uint256 _borrowDepth);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
+
+    constructor(
+        address _want,
+        uint256[] memory _borrowConfig,
+        address _vault,
+        address _unirouter,
+        address _keeper,
+        address _strategist,
+        address _beefyFeeRecipient,
+        address[] memory _outputToNativeRoute,
+        address[] memory _outputToWantRoute
+    ) StratManager(_keeper, _strategist, _unirouter, _vault, _beefyFeeRecipient) public {
+        want = _want;
+
+        borrowRate = _borrowConfig[0];
+        borrowRateMax = _borrowConfig[1];
+        borrowDepth = _borrowConfig[2];
+        minLeverage = _borrowConfig[3];
+
+        (address gToken,,address vToken) = IDataProvider(dataProvider).getReserveTokensAddresses(want);
+        wantTokens = TokenAddresses(want, gToken, vToken);
+
+        output = _outputToNativeRoute[0];
+        native = _outputToNativeRoute[_outputToNativeRoute.length - 1];
+        outputToNativeRoute = _outputToNativeRoute;
+
+        require(_outputToWantRoute[0] == output, "outputToWant[0] != output");
+        require(_outputToWantRoute[_outputToWantRoute.length - 1] == want, "!want");
+        outputToWantRoute = _outputToWantRoute;
+
+        _giveAllowances();
+    }
+
+    // puts the funds to work
+    function deposit() public whenNotPaused {
+        uint256 wantBal = availableWant();
+
+        if (wantBal > 0) {
+            _leverage(wantBal);
+            emit Deposit(balanceOf());
+        }
+    }
+
+    /**
+     * @dev Repeatedly supplies and borrows {want} following the configured {borrowRate} and {borrowDepth}
+     * @param _amount amount of {want} to leverage
+     */
+    function _leverage(uint256 _amount) internal {
+        if (_amount < minLeverage) { return; }
+
+        for (uint i = 0; i < borrowDepth; i++) {
+            ILendingPool(lendingPool).deposit(want, _amount, address(this), 0);
+            _amount = _amount.mul(borrowRate).div(100);
+            if (_amount > 0) {
+                ILendingPool(lendingPool).borrow(want, _amount, INTEREST_RATE_MODE, 0, address(this));
+            }
+        }
+
+        reserves = reserves.add(_amount);
+    }
+
+
+    /**
+     * @dev Incrementally alternates between paying part of the debt and withdrawing part of the supplied
+     * collateral. Continues to do this until it repays the entire debt and withdraws all the supplied {want}
+     * from the system
+     */
+    function _deleverage() internal {
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        (uint256 supplyBal, uint256 borrowBal) = userReserves();
+
+        while (wantBal < borrowBal) {
+            ILendingPool(lendingPool).repay(want, wantBal, INTEREST_RATE_MODE, address(this));
+
+            (supplyBal, borrowBal) = userReserves();
+            uint256 targetSupply = borrowBal.mul(100).div(borrowRate);
+
+            ILendingPool(lendingPool).withdraw(want, supplyBal.sub(targetSupply), address(this));
+            wantBal = IERC20(want).balanceOf(address(this));
+        }
+
+        if (borrowBal > 0) {
+            ILendingPool(lendingPool).repay(want, uint256(-1), INTEREST_RATE_MODE, address(this));
+        }
+        if (supplyBal > 0) {
+            ILendingPool(lendingPool).withdraw(want, type(uint).max, address(this));
+        }
+
+        reserves = 0;
+    }
+
+    /**
+     * @dev Extra safety measure that allows us to manually unwind one level. In case we somehow get into
+     * as state where the cost of unwinding freezes the system. We can manually unwind a few levels
+     * with this function and then 'rebalance()' with new {borrowRate} and {borrowConfig} values.
+     * @param _borrowRate configurable borrow rate in case it's required to unwind successfully
+     */
+    function deleverageOnce(uint _borrowRate) external onlyManager {
+        require(_borrowRate <= borrowRateMax, "!safe");
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        ILendingPool(lendingPool).repay(want, wantBal, INTEREST_RATE_MODE, address(this));
+
+        (uint256 supplyBal, uint256 borrowBal) = userReserves();
+        uint256 targetSupply = borrowBal.mul(100).div(_borrowRate);
+
+        ILendingPool(lendingPool).withdraw(want, supplyBal.sub(targetSupply), address(this));
+
+        wantBal = IERC20(want).balanceOf(address(this));
+        reserves = wantBal;
+    }
+
+    /**
+     * @dev Updates the risk profile and rebalances the vault funds accordingly.
+     * @param _borrowRate percent to borrow on each leverage level.
+     * @param _borrowDepth how many levels to leverage the funds.
+     */
+    function rebalance(uint256 _borrowRate, uint256 _borrowDepth) external onlyManager {
+        require(_borrowRate <= borrowRateMax, "!rate");
+        require(_borrowDepth <= BORROW_DEPTH_MAX, "!depth");
+
+        _deleverage();
+        borrowRate = _borrowRate;
+        borrowDepth = _borrowDepth;
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        _leverage(wantBal);
+
+        StratRebalance(_borrowRate, _borrowDepth);
+    }
+
+    function beforeDeposit() external override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
+        }
+    }
+
+    function harvest() external virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    function managerHarvest() external onlyManager {
+        _harvest(tx.origin);
+    }
+
+    // compounds earnings and charges performance fee
+    function _harvest(address callFeeRecipient) internal whenNotPaused {
+        address[] memory tokens = new address[](2);
+        tokens[0] = wantTokens.gToken;
+        tokens[1] = wantTokens.vToken;
+        incentivesController.claim(address(this), tokens);
+        feeDistribution.exit(true);
+
+        uint256 outputBal = IERC20(output).balanceOf(address(this));
+        if (outputBal > 0) {
+            chargeFees(callFeeRecipient);
+            swapRewards();
+            uint256 wantHarvested = availableWant();
+            deposit();
+
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
+    }
+
+    // performance fees
+    function chargeFees(address callFeeRecipient) internal {
+        uint256 toNative = IERC20(output).balanceOf(address(this)).mul(45).div(1000);
+        IUniswapRouter(unirouter).swapExactTokensForTokens(toNative, 0, outputToNativeRoute, address(this), now);
+
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+
+        uint256 callFeeAmount = nativeBal.mul(callFee).div(MAX_FEE);
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
+
+        uint256 beefyFeeAmount = nativeBal.mul(beefyFee).div(MAX_FEE);
+        IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
+
+        uint256 strategistFee = nativeBal.mul(STRATEGIST_FEE).div(MAX_FEE);
+        IERC20(native).safeTransfer(strategist, strategistFee);
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFee);
+    }
+
+    // swap rewards to {want}
+    function swapRewards() internal {
+        uint256 outputBal = IERC20(output).balanceOf(address(this));
+        IUniswapRouter(unirouter).swapExactTokensForTokens(outputBal, 0, outputToWantRoute, address(this), now);
+    }
+
+    /**
+     * @dev Withdraws funds and sends them back to the vault. It deleverages from venus first,
+     * and then deposits again after the withdraw to make sure it mantains the desired ratio.
+     * @param _amount How much {want} to withdraw.
+     */
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == vault, "!vault");
+
+        uint256 wantBal = availableWant();
+
+        if (wantBal < _amount) {
+            _deleverage();
+            wantBal = IERC20(want).balanceOf(address(this));
+        }
+
+        if (wantBal > _amount) {
+            wantBal = _amount;
+        }
+
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal.mul(withdrawalFee).div(WITHDRAWAL_MAX);
+            wantBal = wantBal.sub(withdrawalFeeAmount);
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+        emit Withdraw(balanceOf());
+
+        if (!paused()) {
+            _leverage(availableWant());
+        }
+    }
+
+    /**
+     * @dev Required for various functions that need to deduct {reserves} from total {want}.
+     * @return how much {want} the contract holds without reserves
+     */
+    function availableWant() public view returns (uint256) {
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        return wantBal.sub(reserves);
+    }
+
+    // return supply and borrow balance
+    function userReserves() public view returns (uint256, uint256) {
+        (uint256 supplyBal,,uint256 borrowBal,,,,,,) = IDataProvider(dataProvider).getUserReserveData(want, address(this));
+        return (supplyBal, borrowBal);
+    }
+
+    // returns the user account data across all the reserves
+    function userAccountData() public view returns (
+        uint256 totalCollateralETH,
+        uint256 totalDebtETH,
+        uint256 availableBorrowsETH,
+        uint256 currentLiquidationThreshold,
+        uint256 ltv,
+        uint256 healthFactor
+    ) {
+        return ILendingPool(lendingPool).getUserAccountData(address(this));
+    }
+
+    // calculate the total underlaying 'want' held by the strat.
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant().add(balanceOfPool());
+    }
+
+    // it calculates how much 'want' this contract holds.
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+    // it calculates how much 'want' the strategy has working in the farm.
+    function balanceOfPool() public view returns (uint256) {
+        (uint256 supplyBal, uint256 borrowBal) = userReserves();
+        return supplyBal.sub(borrowBal);
+    }
+
+    // returns rewards unharvested
+    function rewardsAvailable() public view returns (uint256) {
+        address[] memory tokens = new address[](2);
+        tokens[0] = wantTokens.gToken;
+        tokens[1] = wantTokens.vToken;
+        uint256[] memory reward = incentivesController.claimableReward(address(this), tokens);
+        return reward[0].add(reward[1]).div(4);
+    }
+
+    // native reward amount for calling harvest
+    function callReward() public view returns (uint256) {
+        uint256 outputBal = rewardsAvailable();
+        uint256 nativeOut;
+        if (outputBal > 0) {
+            try IUniswapRouter(unirouter).getAmountsOut(outputBal, outputToNativeRoute)
+                returns (uint256[] memory amountOut)
+            {
+                nativeOut = amountOut[amountOut.length -1];
+            }
+            catch {}
+        }
+
+        return nativeOut.mul(45).div(1000).mul(callFee).div(MAX_FEE);
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+        if (harvestOnDeposit) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
+    }
+
+    // called as part of strat migration. Sends all the available funds back to the vault.
+    function retireStrat() external {
+        require(msg.sender == vault, "!vault");
+
+        _deleverage();
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        IERC20(want).transfer(vault, wantBal);
+    }
+
+    // pauses deposits and withdraws all funds from third party systems.
+    function panic() public onlyManager {
+        _deleverage();
+        pause();
+    }
+
+    function pause() public onlyManager {
+        _pause();
+
+        _removeAllowances();
+    }
+
+    function unpause() external onlyManager {
+        _unpause();
+
+        _giveAllowances();
+
+        deposit();
+    }
+
+    function _giveAllowances() internal {
+        IERC20(want).safeApprove(lendingPool, uint256(-1));
+        IERC20(output).safeApprove(unirouter, uint256(-1));
+    }
+
+    function _removeAllowances() internal {
+        IERC20(want).safeApprove(lendingPool, 0);
+        IERC20(output).safeApprove(unirouter, 0);
+    }
+
+    function outputToNative() public view returns (address[] memory) {
+        return outputToNativeRoute;
+    }
+
+    function outputToWant() public view returns (address[] memory) {
+        return outputToWantRoute;
+    }
+}
--- scripts/vault/deploy-strat-lend.ts
+++ scripts/vault/deploy-strat-lend.ts
@@ -6,44 +6,42 @@ import { verifyContract } from "../../utils/verifyContract";
 import { BeefyChain } from "../../utils/beefyChain";
 
 const {
-  platforms: { spookyswap, beefyfinance },
+  platforms: { pancake, beefyfinance },
   tokens: {
-    SCREAM: { address: SCREAM },
-    fUSDT: { address: fUSDT },
-    WFTM: { address: WFTM },
-    ETH: { address: ETH },
-    WBTC: { address: WBTC },
-    DAI: { address: DAI },
+    VALAS: { address: VALAS },
+    BUSD: { address: BUSD },
+    BNB: { address: BNB },
+    DAI: { address: DAI }
   },
-} = addressBook.fantom;
+} = addressBook.bsc;
 
 const shouldVerifyOnEtherscan = false;
 
-const iToken = web3.utils.toChecksumAddress("0x4565DC3Ef685E4775cdF920129111DdF43B9d882");
-
 const vaultParams = {
-  mooName: "Moo Scream WBTC",
-  mooSymbol: "mooScreamWBTC",
+  mooName: "Moo Valas DAI",
+  mooSymbol: "mooValasDAI",
   delay: 21600,
 };
 
 const strategyParams = {
-  markets: [iToken],
+  want: DAI,
   borrowRate: 72,
   borrowRateMax: 75,
   borrowDepth: 4,
-  minLeverage: 1,
-  outputToNativeRoute: [SCREAM, WFTM],
-  outputToWantRoute: [SCREAM, WFTM, WBTC],
-  unirouter: spookyswap.router,
+  minLeverage: 1000000000,
+  outputToNativeRoute: [VALAS, BNB],
+  outputToWantRoute: [VALAS, BNB, BUSD, DAI],
+  unirouter: pancake.router,
   keeper: beefyfinance.keeper,
-  strategist: "0x010dA5FF62B6e45f89FA7B2d8CEd5a8b5754eC1b",
+  strategist: "0xb2e4A61D99cA58fB8aaC58Bb2F8A59d63f552fC0",
   beefyFeeRecipient: beefyfinance.beefyFeeRecipient,
 };
 
+const borrowConfig = [strategyParams.borrowRate, strategyParams.borrowRateMax, strategyParams.borrowDepth, strategyParams.minLeverage ]
+
 const contractNames = {
   vault: "BeefyVaultV6",
-  strategy: "StrategyScream",
+  strategy: "StrategyValas",
 };
 
 async function main() {
@@ -77,18 +75,15 @@ async function main() {
   await vault.deployed();
 
   const strategyConstructorArguments = [
-    strategyParams.borrowRate,
-    strategyParams.borrowRateMax,
-    strategyParams.borrowDepth,
-    strategyParams.minLeverage,
-    strategyParams.outputToNativeRoute,
-    strategyParams.outputToWantRoute,
-    strategyParams.markets,
+    strategyParams.want,
+    borrowConfig,
     vault.address,
     strategyParams.unirouter,
     strategyParams.keeper,
     strategyParams.strategist,
     strategyParams.beefyFeeRecipient,
+    strategyParams.outputToNativeRoute,
+    strategyParams.outputToWantRoute,
   ];
   const strategy = await Strategy.deploy(...strategyConstructorArguments);
   await strategy.deployed();
@@ -111,6 +106,10 @@ async function main() {
     );
   }
   await setCorrectCallFee(strategy, hardhat.network.name as BeefyChain);
+  console.log();
+
+  console.log(`Transfering Vault Owner to ${beefyfinance.vaultOwner}`)
+  await vault.transferOwnership(beefyfinance.vaultOwner);
   console.log();
 
   await Promise.all(verifyContractsPromises);
