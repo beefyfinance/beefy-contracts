692655c5aecd78a9d0fa4724e17b3dbe81b1bf72
MirthFutures <mirthfutures@gmail.com>
2022-04-19 10:38:37 -0400

    Add Rose and Valas

--- bin/flatten.sh
+++ bin/flatten.sh
@@ -3,11 +3,11 @@
 rm tmp/*.sol
 
 echo "// SPDX-License-Identifier: MIT" > tmp/BeefyVaultV6.sol
-echo "// SPDX-License-Identifier: MIT" > tmp/StrategyBeJoeRewardPool.sol
+echo "// SPDX-License-Identifier: MIT" > tmp/StrategyValasChefLP.sol
 # echo "// SPDX-License-Identifier: MIT" > tmp/TimelockV4.sol
 # echo "// SPDX-License-Identifier: MIT" > tmp/Treasury.sol
 # echo "// SPDX-License-Identifier: MIT" > tmp/Multicall.sol
-echo "// SPDX-License-Identifier: MIT" > tmp/VeJoeStaker.sol
+echo "// SPDX-License-Identifier: MIT" > tmp/StrategyRoseLP.sol
 # echo "// SPDX-License-Identifier: MIT" > tmp/TimelockController.sol
 echo "// SPDX-License-Identifier: MIT" > tmp/RewardPool.sol
 
@@ -17,7 +17,7 @@ echo "// SPDX-License-Identifier: MIT" > tmp/RewardPool.sol
 truffle-flattener contracts/BIFI/vaults/BeefyVaultV6.sol | sed '/SPDX-License-Identifier/d' >> tmp/BeefyVaultV6.sol
 truffle-flattener contracts/BIFI/infra/BeefyRewardPool.sol | sed '/SPDX-License-Identifier/d' >> tmp/BeefyRewardPool.sol
 # truffle-flattener contracts/BIFI/infra/BeefyFeeBatch.sol | sed '/SPDX-License-Identifier/d' >> tmp/Batch.sol
-truffle-flattener contracts/BIFI/strategies/veJOE/StrategyBeJoeRewardPool.sol | sed '/SPDX-License-Identifier/d' >> tmp/StrategyBeJoeRewardPool.sol
-truffle-flattener contracts/BIFI/strategies/veJoe/VeJoeStaker.sol | sed '/SPDX-License-Identifier/d' >> tmp/VeJoeStaker.sol
+truffle-flattener contracts/BIFI/strategies/AaveFork/StrategyValasChefLP.sol | sed '/SPDX-License-Identifier/d' >> tmp/StrategyValasChefLP.sol
+truffle-flattener contracts/BIFI/strategies/Aurora/StrategyRoseLP.sol | sed '/SPDX-License-Identifier/d' >> tmp/StrategyRoseLP.sol
 # truffle-flattener node_modules/@openzeppelin/contracts/access/TimelockController.sol | sed '/SPDX-License-Identifier/d' >> tmp/TimelockController.sol
 
