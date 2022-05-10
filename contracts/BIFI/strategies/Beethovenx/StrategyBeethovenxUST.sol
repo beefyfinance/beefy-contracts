3220d42c3b5ff3a7c259ca1cf77c5e857c1bcc72
kexley <87971154+sircryptomaxi@users.noreply.github.com>
2022-05-10 15:29:57 +0100

    Add ChargeFees event

--- contracts/BIFI/strategies/Beethovenx/StrategyBeethovenxUST.sol
+++ contracts/BIFI/strategies/Beethovenx/StrategyBeethovenxUST.sol
@@ -44,6 +44,7 @@ contract StrategyBeethovenxUST is StratManager, FeeManager {
     event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
     event Deposit(uint256 tvl);
     event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
 
     constructor(
         bytes32[] memory _balancerPoolIds,
@@ -164,8 +165,10 @@ contract StrategyBeethovenxUST is StratManager, FeeManager {
         uint256 beefyFeeAmount = nativeBal.mul(beefyFee).div(MAX_FEE);
         IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
 
-        uint256 strategistFee = nativeBal.mul(STRATEGIST_FEE).div(MAX_FEE);
-        IERC20(native).safeTransfer(strategist, strategistFee);
+        uint256 strategistFeeAmount = nativeBal.mul(STRATEGIST_FEE).div(MAX_FEE);
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
     }
 
     // Adds liquidity to AMM and gets more LP tokens.
