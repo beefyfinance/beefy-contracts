71258004d9f8d6c2be981021c829194b104cd9fd
kexley <sircryptomaxi@gmail.com>
2022-11-05 16:40:28 -0400

    Add Hop Strategy

---
+++ contracts/BIFI/strategies/Hop/StrategyHop.sol
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin-4/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/common/IStableRouter.sol";
+import "../../interfaces/common/IRewardPool.sol";
+import "../Common/StratFeeManager.sol";
+import "../../utils/GasFeeThrottler.sol";
+
+contract StrategyHop is StratFeeManager, GasFeeThrottler {
+    using SafeERC20 for IERC20;
+
+    // Tokens used
+    address public native;
+    address public output;
+    address public want;
+    address public depositToken;
+
+    // Third party contracts
+    address public rewardPool;
+    address public stableRouter;
+    uint256 public depositIndex;
+
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
+
+    constructor(
+        address _want,
+        address _rewardPool,
+        address _stableRouter,
+        CommonAddresses memory _commonAddresses
+    ) StratFeeManager(_commonAddresses) {
+        want = _want;
+        rewardPool = _rewardPool;
+        stableRouter = _stableRouter;
+    }
+
+    // puts the funds to work
+    function deposit() public whenNotPaused {
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal > 0) {
+            IRewardPool(rewardPool).stake(wantBal);
+            emit Deposit(balanceOf());
+        }
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == vault, "!vault");
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal < _amount) {
+            IRewardPool(rewardPool).withdraw(_amount - wantBal);
+            wantBal = IERC20(want).balanceOf(address(this));
+        }
+
+        if (wantBal > _amount) {
+            wantBal = _amount;
+        }
+
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+
+        emit Withdraw(balanceOf());
+    }
+
+    function beforeDeposit() external virtual override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
+        }
+    }
+
+    function harvest() external virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    function managerHarvest() external onlyManager {
+        _harvest(tx.origin);
+    }
+
+    // compounds earnings and charges performance fee
+    function _harvest(address callFeeRecipient) internal {
+        IRewardPool(rewardPool).getReward();
+        uint256 outputBal = IERC20(output).balanceOf(address(this));
+        if (outputBal > 0) {
+            chargeFees(callFeeRecipient);
+            addLiquidity();
+            uint256 wantHarvested = balanceOfWant();
+            deposit();
+
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
+    }
+
+    // performance fees
+    function chargeFees(address callFeeRecipient) internal {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 before = IERC20(native).balanceOf(address(this));
+        _swapToNative(fees.total);
+        uint256 nativeFeeBal = IERC20(native).balanceOf(address(this)) - before;
+
+        uint256 callFeeAmount = nativeFeeBal * fees.call / DIVISOR;
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
+
+        uint256 beefyFeeAmount = nativeFeeBal * fees.beefy / DIVISOR;
+        IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
+
+        uint256 strategistFeeAmount = nativeFeeBal * fees.strategist / DIVISOR;
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
+    }
+
+    // Adds liquidity to AMM and gets more LP tokens.
+    function addLiquidity() internal {
+        _swapToDeposit();
+
+        uint256[] memory inputs = new uint256[](2);
+        inputs[depositIndex] = IERC20(depositToken).balanceOf(address(this));
+        IStableRouter(stableRouter).addLiquidity(inputs, 1, block.timestamp);
+    }
+
+    // calculate the total underlaying 'want' held by the strat.
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant() + balanceOfPool();
+    }
+
+    // it calculates how much 'want' this contract holds.
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+    // it calculates how much 'want' the strategy has working in the farm.
+    function balanceOfPool() public view returns (uint256) {
+        return IRewardPool(rewardPool).balanceOf(address(this));
+    }
+
+    function rewardsAvailable() public view returns (uint256) {
+        return IRewardPool(rewardPool).earned(address(this));
+    }
+
+    function callReward() public view returns (uint256) {
+        uint256 outputBal = rewardsAvailable();
+        uint256 nativeOut;
+
+        nativeOut = _getAmountOut(outputBal);
+
+        IFeeConfig.FeeCategory memory fees = getFees();
+        return nativeOut * fees.total / DIVISOR * fees.call / DIVISOR;
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+
+        if (harvestOnDeposit) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
+    }
+
+    // called as part of strat migration. Sends all the available funds back to the vault.
+    function retireStrat() external {
+        require(msg.sender == vault, "!vault");
+
+        IRewardPool(rewardPool).withdraw(balanceOfPool());
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        IERC20(want).transfer(vault, wantBal);
+    }
+
+    // pauses deposits and withdraws all funds from third party systems.
+    function panic() public onlyManager {
+        pause();
+        IRewardPool(rewardPool).withdraw(balanceOfPool());
+    }
+
+    function pause() public onlyManager {
+        _pause();
+
+        _removeAllowances();
+    }
+
+    function unpause() external onlyManager {
+        _unpause();
+
+        _giveAllowances();
+
+        deposit();
+    }
+
+    function _giveAllowances() internal virtual {
+        IERC20(want).safeApprove(rewardPool, type(uint).max);
+        IERC20(output).safeApprove(unirouter, type(uint).max);
+        IERC20(depositToken).safeApprove(stableRouter, type(uint).max);
+    }
+
+    function _removeAllowances() internal virtual {
+        IERC20(want).safeApprove(rewardPool, 0);
+        IERC20(output).safeApprove(unirouter, 0);
+        IERC20(depositToken).safeApprove(stableRouter, 0);
+    }
+
+    function _swapToNative(uint256 totalFee) internal virtual {}
+
+    function _swapToDeposit() internal virtual {}
+
+    function _getAmountOut(uint256 inputAmount) internal view virtual returns (uint256) {}
+
+    function outputToNative() external view virtual returns (address[] memory) {}
+
+    function outputToDeposit() external view virtual returns (address[] memory) {}
+}
---
+++ contracts/BIFI/strategies/Hop/StrategyHopSolidly.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../../interfaces/common/ISolidlyRouter.sol";
+import "./StrategyHop.sol";
+
+contract StrategyHopSolidly is StrategyHop {
+    // Routes
+    ISolidlyRouter.Routes[] public outputToNativeRoute;
+    ISolidlyRouter.Routes[] public outputToDepositRoute;
+
+    constructor(
+        address _want,
+        address _rewardPool,
+        address _stableRouter,
+        ISolidlyRouter.Routes[] memory _outputToNativeRoute,
+        ISolidlyRouter.Routes[] memory _outputToDepositRoute,
+        CommonAddresses memory _commonAddresses
+    ) StrategyHop(_want, _rewardPool, _stableRouter, _commonAddresses) {
+        for (uint i; i < _outputToNativeRoute.length; ++i) {
+            outputToNativeRoute.push(_outputToNativeRoute[i]);
+        }
+
+        for (uint i; i < _outputToDepositRoute.length; ++i) {
+            outputToDepositRoute.push(_outputToDepositRoute[i]);
+        }
+
+        output = outputToNativeRoute[0].from;
+        native = outputToNativeRoute[outputToNativeRoute.length - 1].to;
+        depositToken = outputToDepositRoute[outputToDepositRoute.length - 1].to;
+        depositIndex = IStableRouter(stableRouter).getTokenIndex(depositToken);
+
+        _giveAllowances();
+    }
+
+    function _swapToNative(uint256 totalFee) internal virtual override {
+        uint256 toNative = IERC20(output).balanceOf(address(this)) * totalFee / DIVISOR;
+        ISolidlyRouter(unirouter).swapExactTokensForTokens(toNative, 0, outputToNativeRoute, address(this), block.timestamp);
+    }
+
+    function _swapToDeposit() internal virtual override {
+        uint256 toDeposit = IERC20(output).balanceOf(address(this));
+        ISolidlyRouter(unirouter).swapExactTokensForTokens(toDeposit, 0, outputToDepositRoute, address(this), block.timestamp);
+    }
+
+    function _getAmountOut(uint256 inputAmount) internal view virtual override returns (uint256) {
+        (uint256 nativeOut,) = ISolidlyRouter(unirouter).getAmountOut(inputAmount, output, native);
+        return nativeOut;
+    }
+
+    function outputToNative() external view virtual override returns (address[] memory) {
+        ISolidlyRouter.Routes[] memory _route = outputToNativeRoute;
+        return _solidlyToRoute(_route);
+    }
+
+    function outputToDeposit() external view virtual override returns (address[] memory) {
+        ISolidlyRouter.Routes[] memory _route = outputToDepositRoute;
+        return _solidlyToRoute(_route);
+    }
+
+    function _solidlyToRoute(ISolidlyRouter.Routes[] memory _route) internal pure virtual returns (address[] memory) {
+        address[] memory route = new address[](_route.length + 1);
+        route[0] = _route[0].from;
+        for (uint i; i < _route.length; ++i) {
+            route[i + 1] = _route[i].to;
+        }
+        return route;
+    }
+}
