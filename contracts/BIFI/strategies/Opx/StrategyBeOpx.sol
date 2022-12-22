2178035d6cda16a325f99727e4934f9743cd4b97
kexley <87971154+sircryptomaxi@users.noreply.github.com>
2022-12-22 20:41:59 +0000

    Add beOPX

---
+++ contracts/BIFI/strategies/Opx/StrategyBeOpx.sol
@@ -0,0 +1,234 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/common/IRewardPool.sol";
+import "../../utils/UniswapV3Utils.sol";
+import "../Common/StratFeeManagerInitializable.sol";
+
+interface IBeToken {
+    function depositAll() external;
+    function harvest() external;
+}
+
+contract StrategyBeOpx is StratFeeManagerInitializable {
+    using SafeERC20 for IERC20;
+
+    // Tokens used
+    address public want;
+    address public native;
+    address public depositToken;
+
+    // Third party contracts
+    address public rewardPool;
+
+    // Route
+    bytes public outputToDepositPath;
+
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
+
+    function initialize(
+        address _want,
+        address _rewardPool,
+        address[] memory _outputToDepositRoute,
+        uint24[] memory _outputToDepositFees,
+        CommonAddresses calldata _commonAddresses
+    ) public initializer {
+        __StratFeeManager_init(_commonAddresses);
+        want = _want;
+        rewardPool = _rewardPool;
+
+        native = _outputToDepositRoute[0];
+        depositToken = _outputToDepositRoute[_outputToDepositRoute.length - 1];
+
+        outputToDepositPath = UniswapV3Utils.routeToPath(_outputToDepositRoute, _outputToDepositFees);
+
+        _giveAllowances();
+    }
+
+    // puts the funds to work
+    function deposit() public whenNotPaused {
+        uint256 wantBal = balanceOfWant();
+
+        if (wantBal > 0) {
+            IRewardPool(rewardPool).stake(wantBal);
+            emit Deposit(balanceOf());
+        }
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == vault, "!vault");
+
+        uint256 wantBal = balanceOfWant();
+
+        if (wantBal < _amount) {
+            IRewardPool(rewardPool).withdraw(_amount - wantBal);
+            wantBal = balanceOfWant();
+        }
+
+        if (wantBal > _amount) {
+            wantBal = _amount;
+        }
+
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+
+        emit Withdraw(balanceOf());
+    }
+
+    function beforeDeposit() external override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
+        }
+    }
+
+    function harvest() external virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    function managerHarvest() external onlyManager {
+        _harvest(tx.origin);
+    }
+
+    // compounds earnings and charges performance fee
+    function _harvest(address callFeeRecipient) internal whenNotPaused {
+        IBeToken(want).harvest();
+        IRewardPool(rewardPool).getReward();
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+        if (nativeBal > 0) {
+            chargeFees(callFeeRecipient);
+            depositBe();
+            uint256 wantHarvested = balanceOfWant();
+            deposit();
+
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
+    }
+
+    // performance fees
+    function chargeFees(address callFeeRecipient) internal {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 feeBal = IERC20(native).balanceOf(address(this)) * fees.total / DIVISOR;
+
+        uint256 callFeeAmount = feeBal * fees.call / DIVISOR;
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
+
+        uint256 beefyFeeAmount = feeBal * fees.beefy / DIVISOR;
+        IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
+
+        uint256 strategistFeeAmount = feeBal * fees.strategist / DIVISOR;
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
+    }
+
+    // Deposit into beToken
+    function depositBe() internal {
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+        if (nativeBal > 0) {
+            UniswapV3Utils.swap(unirouter, outputToDepositPath, nativeBal);
+            IBeToken(want).depositAll();
+        }
+    }
+
+    // calculate the total underlaying 'want' held by the strat.
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant() + balanceOfPool();
+    }
+
+    // it calculates how much 'want' this contract holds.
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+    // it calculates how much 'want' the strategy has working in the farm.
+    function balanceOfPool() public view returns (uint256) {
+        return IRewardPool(rewardPool).balanceOf(address(this));
+    }
+
+    // returns rewards unharvested
+    function rewardsAvailable() public view returns (uint256) {
+        return IRewardPool(rewardPool).earned(address(this));
+    }
+
+    // native reward amount for calling harvest
+    function callReward() public view returns (uint256) {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 nativeBal = rewardsAvailable();
+
+        return nativeBal * fees.total / DIVISOR * fees.call / DIVISOR;
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+        if (harvestOnDeposit == true) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
+    }
+
+    // called as part of strat migration. Sends all the available funds back to the vault.
+    function retireStrat() external {
+        require(msg.sender == vault, "!vault");
+
+        IRewardPool(rewardPool).withdraw(balanceOfPool());
+
+        uint256 wantBal = balanceOfWant();
+        IERC20(want).transfer(vault, wantBal);
+    }
+
+    // pauses deposits and withdraws all funds from third party systems.
+    function panic() external onlyManager {
+        pause();
+        IRewardPool(rewardPool).withdraw(balanceOfPool());
+    }
+
+    function pause() public onlyManager {
+        _pause();
+
+        _removeAllowances();
+    }
+
+    function unpause() external onlyManager {
+        _unpause();
+
+        _giveAllowances();
+
+        deposit();
+    }
+
+    function _giveAllowances() internal {
+        IERC20(want).safeApprove(rewardPool, type(uint).max);
+        IERC20(native).safeApprove(unirouter, type(uint).max);
+        IERC20(depositToken).safeApprove(want, type(uint).max);
+    }
+
+    function _removeAllowances() internal {
+        IERC20(want).safeApprove(rewardPool, 0);
+        IERC20(native).safeApprove(unirouter, 0);
+        IERC20(depositToken).safeApprove(want, 0);
+    }
+
+    function outputToDeposit() external view returns (address[] memory) {
+        return UniswapV3Utils.pathToRoute(outputToDepositPath);
+    }
+}
