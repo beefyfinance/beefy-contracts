6c8c2031f51d425b9091d0b8f5faaafdc61c8bb0
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2023-02-13 13:26:37 -0500

    Add StrategyBalancerMultiRewardChefUniV2

---
+++ contracts/BIFI/strategies/Balancer/StrategyBalancerMultiRewardChefUniV2.sol
@@ -0,0 +1,364 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin-4/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/common/IUniswapRouterETH.sol";
+import "../../interfaces/beethovenx/IBeethovenxChef.sol";
+import "../../interfaces/beethovenx/IBalancerVault.sol";
+import "../../interfaces/curve/IRewardsGauge.sol";
+import "../../interfaces/curve/IStreamer.sol";
+import "../../interfaces/curve/IHelper.sol";
+import "../Common/StratFeeManagerInitializable.sol";
+import "./BalancerActionsLib.sol";
+import "./BeefyBalancerStructs.sol";
+
+interface IBalancerPool {
+    function getPoolId() external view returns (bytes32);
+}
+
+contract StrategyBalancerMultiRewardChefUniV2 is StratFeeManagerInitializable {
+    using SafeERC20 for IERC20;
+
+    // Tokens used
+    address public want;
+    address public output;
+    address public native;
+    
+    BeefyBalancerStructs.Input public input;
+
+    // Third party contracts
+    address public chef;
+    uint256 public poolId;
+    
+    BeefyBalancerStructs.BatchSwapStruct[] public nativeToInputRoute;
+    BeefyBalancerStructs.BatchSwapStruct[] public outputToNativeRoute;
+    address[] public nativeToInputAssets;
+    address[] public outputToNativeAssets;
+
+    mapping(address => BeefyBalancerStructs.Reward) public rewards;
+    address[] public rewardTokens;
+    
+    address public uniswapRouter = address(0xF491e7B69E4244ad4002BC14e878a34207E38c29);
+
+    IBalancerVault.SwapKind public swapKind = IBalancerVault.SwapKind.GIVEN_IN;
+    IBalancerVault.FundManagement public funds;
+
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
+
+    function initialize(
+        address _want,
+        bool[] calldata _switches,
+        BeefyBalancerStructs.BatchSwapStruct[] calldata _nativeToInputRoute,
+        BeefyBalancerStructs.BatchSwapStruct[] calldata _outputToNativeRoute,
+        address[][] calldata _assets,
+        address _chef,
+        uint256 _poolId,
+        CommonAddresses calldata _commonAddresses
+      ) public initializer  {
+        __StratFeeManager_init(_commonAddresses);
+        
+        for (uint i; i < _nativeToInputRoute.length;) {
+            nativeToInputRoute.push(_nativeToInputRoute[i]);
+            unchecked {
+                ++i;
+            }
+        }
+
+        for (uint j; j < _outputToNativeRoute.length;) {
+            outputToNativeRoute.push(_outputToNativeRoute[j]);
+            unchecked {
+                ++j;
+            }
+        }
+
+        outputToNativeAssets = _assets[0];
+        nativeToInputAssets = _assets[1];
+        output = outputToNativeAssets[0];
+        native = nativeToInputAssets[0];
+        input.input = nativeToInputAssets[nativeToInputAssets.length - 1];
+        input.isComposable = _switches[0];
+       
+        funds = IBalancerVault.FundManagement(address(this), false, payable(address(this)), false);
+
+        chef = _chef;
+        poolId = _poolId;
+        input.isBeets = _switches[1];
+
+        want = _want;
+        _giveAllowances();
+    }
+
+    // puts the funds to work
+    function deposit() public whenNotPaused {
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal > 0) {
+            IBeethovenxChef(chef).deposit(poolId, wantBal, address(this));
+            emit Deposit(balanceOf());
+        }
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == vault, "!vault");
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal < _amount) {
+            IBeethovenxChef(chef).withdrawAndHarvest(poolId, _amount - wantBal, address(this));
+            wantBal = IERC20(want).balanceOf(address(this));
+        }
+
+        if (wantBal > _amount) {
+            wantBal = _amount;
+        }
+
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+
+        emit Withdraw(balanceOf());
+    }
+
+    function beforeDeposit() external override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
+        }
+    }
+
+    function harvest() external virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    function managerHarvest() external onlyManager {
+        _harvest(tx.origin);
+    }
+
+    // compounds earnings and charges performance fee
+    function _harvest(address callFeeRecipient) internal whenNotPaused {
+        IBeethovenxChef(chef).harvest(poolId, address(this));
+     
+        swapRewardsToNative();
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+
+        if (nativeBal > 0) {
+            chargeFees(callFeeRecipient);
+            addLiquidity();
+            uint256 wantHarvested = balanceOfWant();
+            deposit();
+
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
+    }
+
+    function swapRewardsToNative() internal {
+        uint256 outputBal = IERC20(output).balanceOf(address(this));
+        if (outputBal > 0) {
+            IBalancerVault.BatchSwapStep[] memory _swaps = BalancerActionsLib.buildSwapStructArray(outputToNativeRoute, outputBal);
+            BalancerActionsLib.balancerSwap(unirouter, swapKind, _swaps, outputToNativeAssets, funds, int256(outputBal));
+        }
+        // extras
+        for (uint i; i < rewardTokens.length; i++) {
+            uint bal = IERC20(rewardTokens[i]).balanceOf(address(this));
+            if (bal >= rewards[rewardTokens[i]].minAmount) {
+                if (rewards[rewardTokens[i]].assets[0] != address(0)) {
+                    BeefyBalancerStructs.BatchSwapStruct[] memory swapInfo = new BeefyBalancerStructs.BatchSwapStruct[](rewards[rewardTokens[i]].assets.length - 1);
+                    for (uint j; j < rewards[rewardTokens[i]].assets.length - 1;) {
+                        swapInfo[j] = rewards[rewardTokens[i]].swapInfo[j];
+                        unchecked {
+                            ++j;
+                        }
+                    }
+                    IBalancerVault.BatchSwapStep[] memory _swaps = BalancerActionsLib.buildSwapStructArray(swapInfo, bal);
+                    BalancerActionsLib.balancerSwap(unirouter, swapKind, _swaps, rewards[rewardTokens[i]].assets, funds, int256(bal));
+                } else {
+                    IUniswapRouterETH(uniswapRouter).swapExactTokensForTokens(bal, 0, rewards[rewardTokens[i]].assets, address(this), block.timestamp);
+                }
+            }
+        }
+    }
+
+    // performance fees
+    function chargeFees(address callFeeRecipient) internal {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 nativeBal = IERC20(native).balanceOf(address(this)) * fees.total / DIVISOR;
+
+        uint256 callFeeAmount = nativeBal * fees.call / DIVISOR;
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
+
+        uint256 beefyFeeAmount = nativeBal * fees.beefy / DIVISOR;
+        IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
+
+        uint256 strategistFeeAmount = nativeBal * fees.strategist / DIVISOR;
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
+    }
+
+    // Adds liquidity to AMM and gets more LP tokens.
+    function addLiquidity() internal {
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+        if (native != input.input) {
+            IBalancerVault.BatchSwapStep[] memory _swaps = BalancerActionsLib.buildSwapStructArray(nativeToInputRoute, nativeBal);
+            BalancerActionsLib.balancerSwap(unirouter, swapKind, _swaps, nativeToInputAssets, funds, int256(nativeBal));
+        }
+
+        uint256 inputBal = IERC20(input.input).balanceOf(address(this));
+        BalancerActionsLib.balancerJoin(unirouter, IBalancerPool(want).getPoolId(), input.input, inputBal);
+    }
+
+    // calculate the total underlaying 'want' held by the strat.
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant() + balanceOfPool();
+    }
+
+    // it calculates how much 'want' this contract holds.
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+    // it calculates how much 'want' the strategy has working in the farm.
+    function balanceOfPool() public view returns (uint256) {
+        (uint256 _amount,) = IBeethovenxChef(chef).userInfo(poolId, address(this));
+        return _amount;
+    }
+
+    // returns rewards unharvested
+    function rewardsAvailable() public view returns (uint256) {
+        return IBeethovenxChef(chef).pendingBeets(poolId, address(this));
+    }
+
+    // native reward amount for calling harvest
+    function callReward() public pure returns (uint256) {
+       return 0; // multiple swap providers with no easy way to estimate native output. 
+    }
+
+    function addRewardToken(address _token, BeefyBalancerStructs.BatchSwapStruct[] memory _swapInfo, address[] memory _assets, bytes calldata _routeToNative, uint _minAmount) external onlyOwner {
+        require(_token != want, "!want");
+        require(_token != native, "!native");
+        if (_assets[0] != address(0)) {
+            IERC20(_token).safeApprove(unirouter, 0);
+            IERC20(_token).safeApprove(unirouter, type(uint).max);
+        } else {
+            IERC20(_token).safeApprove(uniswapRouter, 0);
+            IERC20(_token).safeApprove(uniswapRouter, type(uint).max);
+        }
+
+        rewards[_token].assets = _assets;
+        rewards[_token].routeToNative = _routeToNative;
+        rewards[_token].minAmount = _minAmount;
+
+        for (uint i; i < _swapInfo.length; ) {
+            rewards[_token].swapInfo[i].poolId = _swapInfo[i].poolId;
+            rewards[_token].swapInfo[i].assetInIndex = _swapInfo[i].assetInIndex;
+            rewards[_token].swapInfo[i].assetOutIndex = _swapInfo[i].assetOutIndex;
+            unchecked {
+                ++i;
+            }
+        }
+        rewardTokens.push(_token);
+    }
+
+    function resetRewardTokens() external onlyManager {
+        for (uint i; i < rewardTokens.length;) {
+            delete rewards[rewardTokens[i]];
+            unchecked { ++i; }
+        }
+        delete rewardTokens;
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+
+        if (harvestOnDeposit) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
+    }
+
+    // called as part of strat migration. Sends all the available funds back to the vault.
+    function retireStrat() external {
+        require(msg.sender == vault, "!vault");
+
+        IBeethovenxChef(chef).emergencyWithdraw(poolId, address(this));
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        IERC20(want).transfer(vault, wantBal);
+    }
+
+    // pauses deposits and withdraws all funds from third party systems.
+    function panic() public onlyManager {
+        pause();
+        IBeethovenxChef(chef).emergencyWithdraw(poolId, address(this));
+    }
+
+    function pause() public onlyManager {
+        _pause();
+
+        _removeAllowances();
+    }
+
+    function unpause() external onlyManager {
+        _unpause();
+
+        _giveAllowances();
+
+        deposit();
+    }
+
+    function _giveAllowances() internal {
+        IERC20(want).safeApprove(chef, type(uint).max);
+        IERC20(output).safeApprove(unirouter, type(uint).max);
+        IERC20(native).safeApprove(unirouter, type(uint).max);
+        if (!input.isComposable) {
+            IERC20(input.input).safeApprove(unirouter, 0);
+            IERC20(input.input).safeApprove(unirouter, type(uint).max);
+        }
+        if (rewardTokens.length != 0) {
+            for (uint i; i < rewardTokens.length; ++i) {
+                if (rewards[rewardTokens[i]].assets[0] != address(0)) {
+                    IERC20(rewardTokens[i]).safeApprove(unirouter, 0);
+                    IERC20(rewardTokens[i]).safeApprove(unirouter, type(uint).max);
+                } else {
+                    IERC20(rewardTokens[i]).safeApprove(uniswapRouter, 0);
+                    IERC20(rewardTokens[i]).safeApprove(uniswapRouter, type(uint).max);
+                }
+            }
+        }
+    }
+
+    function _removeAllowances() internal {
+        IERC20(want).safeApprove(chef, 0);
+        IERC20(output).safeApprove(unirouter, 0);
+        IERC20(native).safeApprove(unirouter, 0);
+        if (!input.isComposable) {
+            IERC20(input.input).safeApprove(unirouter, 0);
+        }
+        if (rewardTokens.length != 0) { 
+            for (uint i; i < rewardTokens.length; ++i) {
+                if (rewards[rewardTokens[i]].assets[0] != address(0)) {
+                    IERC20(rewardTokens[i]).safeApprove(unirouter, 0);
+                } else {
+                    IERC20(rewardTokens[i]).safeApprove(uniswapRouter, 0);
+                }
+            }
+        }
+    }
+}
