6c5bc6a01eab69d9820c29b5e2dbd8ef74c18784
kexley <sircryptomaxi@gmail.com>
2023-04-14 12:09:08 -0400

    BeefyVaultV7Native update

--- contracts/BIFI/vaults/BeefyVaultV7Native.sol
+++ contracts/BIFI/vaults/BeefyVaultV7Native.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
@@ -203,7 +204,7 @@ contract BeefyVaultV7Native is ERC20Upgradeable, OwnableUpgradeable, ReentrancyG
         }
 
         IWrappedNative(address(want())).withdraw(r);
-        msg.sender.transfer(r);
+        payable(msg.sender).transfer(r);
     }
 
     /** 
