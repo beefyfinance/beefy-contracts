613d4689b00494b9112d044afe400cdd60eb797f
kexley <sircryptomaxi@gmail.com>
2023-06-12 17:17:47 +0100

    Update StrategyNewQuick

--- contracts/BIFI/strategies/QuickSwap/StrategyNewQuick.sol
+++ contracts/BIFI/strategies/QuickSwap/StrategyNewQuick.sol
@@ -1,37 +1,27 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "../../interfaces/common/IUniswapRouterETH.sol";
-import "../../interfaces/common/IUniswapV2Pair.sol";
+import "../../utils/AlgebraUtils.sol";
 import "../../interfaces/common/IRewardPool.sol";
-import "../../interfaces/quick/IQuickConverter.sol";
-import "../Common/StratManager.sol";
-import "../Common/FeeManager.sol";
+import "../Common/StratFeeManagerInitializable.sol";
 
-contract StrategyNewQuick is StratManager, FeeManager {
+contract StrategyNewQuick is StratFeeManagerInitializable {
     using SafeERC20 for IERC20;
-    using SafeMath for uint256;
 
     // Tokens used
     address public native;
     address public output;
     address public want;
-    address public oldWant;
 
     // Third party contracts
     address public rewardPool;
-    address public quickConverter;
-    bool public convert = true;
 
     // Routes
-    address[] public outputToNativeRoute;
-    address[] public outputToWantRoute;
-    address[] public outputToOldWantRoute;
+    bytes public outputToNativePath;
+    bytes public outputToWantPath;
 
     bool public harvestOnDeposit;
     uint256 public lastHarvest;
@@ -41,34 +31,23 @@ contract StrategyNewQuick is StratManager, FeeManager {
     event Withdraw(uint256 tvl);
     event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
 
-    constructor(
-        address _want,
+    function initialize(
         address _rewardPool,
-        address _quickConverter,
-        address _vault,
-        address _unirouter,
-        address _keeper,
-        address _strategist,
-        address _beefyFeeRecipient,
-        address[] memory _outputToNativeRoute,
-        address[] memory _outputToWantRoute,
-        address[] memory _outputToOldWantRoute
-    ) StratManager(_keeper, _strategist, _unirouter, _vault, _beefyFeeRecipient) public {
-        want = _want;
+        address[] calldata _outputToNativeRoute,
+        address[] calldata _outputToWantRoute,
+        CommonAddresses calldata _commonAddresses
+    ) public initializer {
+        __StratFeeManager_init(_commonAddresses);
         rewardPool = _rewardPool;
-        quickConverter = _quickConverter;
 
         output = _outputToNativeRoute[0];
         native = _outputToNativeRoute[_outputToNativeRoute.length - 1];
-        oldWant = _outputToOldWantRoute[_outputToOldWantRoute.length - 1];
-        outputToNativeRoute = _outputToNativeRoute;
+        want = _outputToWantRoute[_outputToWantRoute.length - 1];
 
-        require(_outputToWantRoute[0] == output, "toDeposit[0] != output");
-        require(_outputToWantRoute[_outputToWantRoute.length - 1] == want, "!want");
-        outputToWantRoute = _outputToWantRoute;
+        outputToNativePath = AlgebraUtils.routeToPath(_outputToNativeRoute);
 
-        require(_outputToOldWantRoute[0] == output, "toOldWant[0] != output");
-        outputToOldWantRoute = _outputToOldWantRoute;
+        require(_outputToWantRoute[0] == output, "toWant[0] != output");
+        outputToWantPath = AlgebraUtils.routeToPath(_outputToWantRoute);
 
         _giveAllowances();
     }
@@ -89,7 +68,7 @@ contract StrategyNewQuick is StratManager, FeeManager {
         uint256 wantBal = balanceOfWant();
 
         if (wantBal < _amount) {
-            IRewardPool(rewardPool).withdraw(_amount.sub(wantBal));
+            IRewardPool(rewardPool).withdraw(_amount - wantBal);
             wantBal = balanceOfWant();
         }
 
@@ -98,8 +77,8 @@ contract StrategyNewQuick is StratManager, FeeManager {
         }
 
         if (tx.origin != owner() && !paused()) {
-            uint256 withdrawalFeeAmount = wantBal.mul(withdrawalFee).div(WITHDRAWAL_MAX);
-            wantBal = wantBal.sub(withdrawalFeeAmount);
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
         }
 
         IERC20(want).safeTransfer(vault, wantBal);
@@ -144,18 +123,19 @@ contract StrategyNewQuick is StratManager, FeeManager {
 
     // performance fees
     function chargeFees(address callFeeRecipient) internal {
-        uint256 toNative = IERC20(output).balanceOf(address(this)).mul(45).div(1000);
-        IUniswapRouterETH(unirouter).swapExactTokensForTokens(toNative, 0, outputToNativeRoute, address(this), now);
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 toNative = IERC20(output).balanceOf(address(this)) * fees.total / DIVISOR;
 
-        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+        AlgebraUtils.swap(unirouter, outputToNativePath, toNative);
+        uint256 feeBal = IERC20(native).balanceOf(address(this));
 
-        uint256 callFeeAmount = nativeBal.mul(callFee).div(MAX_FEE);
+        uint256 callFeeAmount = feeBal * fees.call / DIVISOR;
         IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
 
-        uint256 beefyFeeAmount = nativeBal.mul(beefyFee).div(MAX_FEE);
+        uint256 beefyFeeAmount = feeBal * fees.beefy / DIVISOR;
         IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
 
-        uint256 strategistFeeAmount = nativeBal.mul(STRATEGIST_FEE).div(MAX_FEE);
+        uint256 strategistFeeAmount = feeBal * fees.strategist / DIVISOR;
         IERC20(native).safeTransfer(strategist, strategistFeeAmount);
 
         emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
@@ -165,19 +145,13 @@ contract StrategyNewQuick is StratManager, FeeManager {
     function swapRewards() internal {
         if (want != output) {
             uint256 outputBal = IERC20(output).balanceOf(address(this));
-            if (convert) {
-                IUniswapRouterETH(unirouter).swapExactTokensForTokens(outputBal, 0, outputToOldWantRoute, address(this), now);
-                uint256 oldWantBal = IERC20(oldWant).balanceOf(address(this));
-                IQuickConverter(quickConverter).quickToQuickX(oldWantBal);
-            } else {
-                IUniswapRouterETH(unirouter).swapExactTokensForTokens(outputBal, 0, outputToWantRoute, address(this), now);
-            }
+            AlgebraUtils.swap(unirouter, outputToWantPath, outputBal);
         }
     }
 
     // calculate the total underlaying 'want' held by the strat.
     function balanceOf() public view returns (uint256) {
-        return balanceOfWant().add(balanceOfPool());
+        return balanceOfWant() + balanceOfPool();
     }
 
     // it calculates how much 'want' this contract holds.
@@ -196,15 +170,8 @@ contract StrategyNewQuick is StratManager, FeeManager {
     }
 
     // native reward amount for calling harvest
-    function callReward() public view returns (uint256) {
-        uint256 outputBal = rewardsAvailable();
-        uint256 nativeOut;
-        if (outputBal > 0) {
-            uint256[] memory amountOut = IUniswapRouterETH(unirouter).getAmountsOut(outputBal, outputToNativeRoute);
-            nativeOut = amountOut[amountOut.length -1];
-        }
-
-        return nativeOut.mul(45).div(1000).mul(callFee).div(MAX_FEE);
+    function callReward() public pure returns (uint256) {
+        return 0;
     }
 
     // called as part of strat migration. Sends all the available funds back to the vault.
@@ -248,58 +215,44 @@ contract StrategyNewQuick is StratManager, FeeManager {
     }
 
     function outputToNative() public view returns (address[] memory) {
-        return outputToNativeRoute;
+        return AlgebraUtils.pathToRoute(outputToNativePath);
     }
 
     function outputToWant() public view returns (address[] memory) {
-        return outputToWantRoute;
-    }
-
-    function outputToOldWant() public view returns (address[] memory) {
-        return outputToWantRoute;
+        return AlgebraUtils.pathToRoute(outputToWantPath);
     }
 
     function _giveAllowances() internal {
-        IERC20(want).safeApprove(rewardPool, uint256(-1));
-        IERC20(output).safeApprove(unirouter, uint256(-1));
-        IERC20(oldWant).safeApprove(quickConverter, uint256(-1));
+        IERC20(want).safeApprove(rewardPool, type(uint).max);
+        IERC20(output).safeApprove(unirouter, type(uint).max);
     }
 
     function _removeAllowances() internal {
         IERC20(want).safeApprove(rewardPool, 0);
         IERC20(output).safeApprove(unirouter, 0);
-        IERC20(oldWant).safeApprove(quickConverter, 0);
-    }
-
-    function setConvert(bool _convert) external onlyManager {
-        convert = _convert;
-
-        if (convert) {
-            IERC20(oldWant).safeApprove(quickConverter, 0);
-            IERC20(oldWant).safeApprove(quickConverter, uint256(-1));
-        } else {
-            IERC20(oldWant).safeApprove(quickConverter, 0);
-        }
     }
 
     function swapRewardPool(
         address _rewardPool,
-        address[] memory _outputToNativeRoute,
-        address[] memory _outputToWantRoute
+        address[] calldata _outputToNativeRoute,
+        address[] calldata _outputToWantRoute
     ) external onlyOwner {
         require(want == IRewardPool(_rewardPool).stakingToken(), "Proposal not valid for this Vault");
         require((_outputToNativeRoute[0] == IRewardPool(_rewardPool).rewardsToken())
-            && (_outputToWantRoute[0] == IRewardPool(_rewardPool).rewardsToken()),
-            "Proposed output in route is not valid");
-        require(_outputToWantRoute[_outputToWantRoute.length - 1] == want, "Proposed want in route is not valid");
+            && (want != IRewardPool(_rewardPool).rewardsToken()),
+            "Proposed output in route is not valid"
+        );
+        require(want == _outputToWantRoute[_outputToWantRoute.length - 1], 
+            "Proposed want in route not valid"
+        );
 
         IRewardPool(rewardPool).withdraw(balanceOfPool());
         _removeAllowances();
 
         rewardPool = _rewardPool;
         output = _outputToNativeRoute[0];
-        outputToNativeRoute = _outputToNativeRoute;
-        outputToWantRoute = _outputToWantRoute;
+        outputToNativePath = AlgebraUtils.routeToPath(_outputToNativeRoute);
+        outputToWantPath = AlgebraUtils.routeToPath(_outputToWantRoute);
 
         _giveAllowances();
         IRewardPool(rewardPool).stake(balanceOfWant());
