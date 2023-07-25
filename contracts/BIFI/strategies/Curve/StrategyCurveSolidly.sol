b7da99e788ae9314f7aa59cdb495cbe4eca44187
RM <roman-monk@users.noreply.github.com>
2023-07-25 12:40:58 +0700

    StrategyCurveSolidly

---
+++ contracts/BIFI/strategies/Curve/StrategyCurveSolidly.sol
@@ -0,0 +1,279 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin-4/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/common/ISolidlyRouter.sol";
+import "../../interfaces/common/IWrappedNative.sol";
+import "../../interfaces/curve/ICurveSwap.sol";
+import "../../interfaces/curve/IGaugeFactory.sol";
+import "../../interfaces/curve/IRewardsGauge.sol";
+import "../Common/StratFeeManagerInitializable.sol";
+
+contract StrategyCurveSolidly is  StratFeeManagerInitializable  {
+    using SafeERC20 for IERC20;
+
+    // Tokens used
+    address public want; // curve lpToken
+    address public native;
+    address public output;
+    address public depositToken;
+
+    // Third party contracts
+    address public rewardsGauge;
+    address public pool;
+    uint public poolSize;
+    uint public depositIndex;
+    bool public useUnderlying;
+    bool public useMetapool;
+
+    // Routes
+    ISolidlyRouter.Routes[] public nativeToDepositRoute;
+
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
+
+    function initialize(
+        address _want,
+        address _gauge,
+        address _pool,
+        uint[] memory _params, // [poolSize, depositIndex, useUnderlying, useMetapool]
+        ISolidlyRouter.Routes[] calldata _nativeToDeposit,
+        CommonAddresses calldata _commonAddresses
+    ) public initializer {
+        __StratFeeManager_init(_commonAddresses);
+        want = _want;
+        rewardsGauge = _gauge;
+        pool = _pool;
+        poolSize = _params[0];
+        depositIndex = _params[1];
+        useUnderlying = _params[2] > 0;
+        useMetapool = _params[3] > 0;
+
+        for (uint i; i < _nativeToDeposit.length; ++i) {
+            nativeToDepositRoute.push(_nativeToDeposit[i]);
+        }
+
+        native = _nativeToDeposit[0].from;
+        depositToken = _nativeToDeposit[_nativeToDeposit.length - 1].to;
+        output = native;
+
+        _giveAllowances();
+    }
+
+    // puts the funds to work
+    function deposit() public whenNotPaused {
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal > 0) {
+            IRewardsGauge(rewardsGauge).deposit(wantBal);
+            emit Deposit(balanceOf());
+        }
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == vault, "!vault");
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal < _amount) {
+            IRewardsGauge(rewardsGauge).withdraw(_amount - wantBal);
+            wantBal = IERC20(want).balanceOf(address(this));
+        }
+
+        if (wantBal > _amount) {
+            wantBal = _amount;
+        }
+
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+
+        emit Withdraw(balanceOf());
+    }
+
+    function beforeDeposit() external override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
+        }
+    }
+
+    function harvest() external virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    // compounds earnings and charges performance fee
+    function _harvest(address callFeeRecipient) internal whenNotPaused {
+        IRewardsGauge(rewardsGauge).claim_rewards(address(this));
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+        if (nativeBal > 0) {
+            chargeFees(callFeeRecipient);
+            addLiquidity();
+            uint256 wantHarvested = balanceOfWant();
+            deposit();
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
+    }
+
+    // performance fees
+    function chargeFees(address callFeeRecipient) internal {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 nativeBal = IERC20(native).balanceOf(address(this)) * fees.total / DIVISOR;
+
+        uint256 callFeeAmount = nativeBal * fees.call / DIVISOR;
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
+
+        uint256 beefyFeeAmount = nativeBal * fees.beefy / DIVISOR;
+        IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
+
+        uint256 strategistFeeAmount = nativeBal * fees.strategist / DIVISOR;
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
+    }
+
+    // Adds liquidity to AMM and gets more LP tokens.
+    function addLiquidity() internal {
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+        if (depositToken != native) {
+            ISolidlyRouter(unirouter).swapExactTokensForTokens(nativeBal, 0, nativeToDepositRoute, address(this), block.timestamp);
+        }
+
+        uint256 depositBal = IERC20(depositToken).balanceOf(address(this));
+
+        if (poolSize == 2) {
+            uint256[2] memory amounts;
+            amounts[depositIndex] = depositBal;
+            if (useUnderlying) ICurveSwap(pool).add_liquidity(amounts, 0, true);
+            else ICurveSwap(pool).add_liquidity(amounts, 0);
+        } else if (poolSize == 3) {
+            uint256[3] memory amounts;
+            amounts[depositIndex] = depositBal;
+            if (useUnderlying) ICurveSwap(pool).add_liquidity(amounts, 0, true);
+            else if (useMetapool) ICurveSwap(pool).add_liquidity(want, amounts, 0);
+            else ICurveSwap(pool).add_liquidity(amounts, 0);
+        } else if (poolSize == 4) {
+            uint256[4] memory amounts;
+            amounts[depositIndex] = depositBal;
+            if (useMetapool) ICurveSwap(pool).add_liquidity(want, amounts, 0);
+            else ICurveSwap(pool).add_liquidity(amounts, 0);
+        } else if (poolSize == 5) {
+            uint256[5] memory amounts;
+            amounts[depositIndex] = depositBal;
+            ICurveSwap(pool).add_liquidity(amounts, 0);
+        }
+    }
+
+    // calculate the total underlaying 'want' held by the strat.
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant() + balanceOfPool();
+    }
+
+    // it calculates how much 'want' this contract holds.
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+    // it calculates how much 'want' the strategy has working in the farm.
+    function balanceOfPool() public view returns (uint256) {
+        return IRewardsGauge(rewardsGauge).balanceOf(address(this));
+    }
+
+    function nativeToDeposit() external view returns (address[] memory) {
+        return _solidlyToRoute(nativeToDepositRoute);
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+        if (harvestOnDeposit) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
+    }
+
+    // returns rewards unharvested
+    function rewardsAvailable() public view returns (uint256) {
+        return IRewardsGauge(rewardsGauge).claimable_reward(address(this), output);
+    }
+
+    // native reward amount for calling harvest
+    function callReward() public view returns (uint256) {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        return rewardsAvailable() * fees.total / DIVISOR * fees.call / DIVISOR;
+    }
+
+    // called as part of strat migration. Sends all the available funds back to the vault.
+    function retireStrat() external {
+        require(msg.sender == vault, "!vault");
+
+        IRewardsGauge(rewardsGauge).withdraw(balanceOfPool());
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        IERC20(want).transfer(vault, wantBal);
+    }
+
+    // pauses deposits and withdraws all funds from third party systems.
+    function panic() public onlyManager {
+        pause();
+        IRewardsGauge(rewardsGauge).withdraw(balanceOfPool());
+    }
+
+    function pause() public onlyManager {
+        _pause();
+
+        _removeAllowances();
+    }
+
+    function unpause() external onlyManager {
+        _unpause();
+
+        _giveAllowances();
+
+        deposit();
+    }
+
+    function _giveAllowances() internal {
+        IERC20(want).safeApprove(rewardsGauge, type(uint).max);
+        IERC20(native).safeApprove(unirouter, type(uint).max);
+        IERC20(depositToken).safeApprove(pool, type(uint).max);
+
+        IERC20(output).safeApprove(unirouter, 0);
+        IERC20(output).safeApprove(unirouter, type(uint).max);
+
+    }
+
+    function _removeAllowances() internal {
+        IERC20(want).safeApprove(rewardsGauge, 0);
+        IERC20(native).safeApprove(unirouter, 0);
+        IERC20(output).safeApprove(unirouter, 0);
+        IERC20(depositToken).safeApprove(pool, 0);
+    }
+
+    function _solidlyToRoute(ISolidlyRouter.Routes[] memory _route) internal pure returns (address[] memory) {
+        address[] memory route = new address[](_route.length + 1);
+        route[0] = _route[0].from;
+        for (uint i; i < _route.length; ++i) {
+            route[i + 1] = _route[i].to;
+        }
+        return route;
+    }
+
+    receive () external payable {}
+}
