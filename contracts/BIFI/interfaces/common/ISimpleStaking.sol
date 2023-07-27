a24a786b50c3d923a5873c3041331034f02ace80
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2023-07-27 14:42:12 +0100

    Update OOE Strat

--- contracts/BIFI/interfaces/common/ISimpleStaking.sol
+++ contracts/BIFI/interfaces/common/ISimpleStaking.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 interface ISimpleStaking {
     function deposit(uint256 amount) external;
     function withdraw(uint256 amount) external;
     function claimReward(address[] memory user) external;
     function userCollateral(address account) external view returns (uint256);
+    function pendingReward(address user) external view returns (uint256);
 }
--- contracts/BIFI/strategies/OpeanOcean/StrategyOpeanOceanRewardPoolLP.sol
+++ contracts/BIFI/strategies/OpeanOcean/StrategyOpeanOceanRewardPoolLP.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "../../interfaces/common/IERC20Extended.sol";
 import "../../interfaces/common/IUniswapRouterETH.sol";
 import "../../interfaces/common/IUniswapV2Pair.sol";
 import "../../interfaces/common/ISimpleStaking.sol";
-import "../../utils/GasThrottler.sol";
-import "../Common/StratManager.sol";
-import "../Common/FeeManager.sol";
+import "../Common/StratFeeManagerInitializable.sol";
+import "../../utils/GasFeeThrottler.sol";
 
-contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottler {
+contract StrategyOpeanOceanRewardPoolLP is StratFeeManagerInitializable, GasFeeThrottler {
     using SafeERC20 for IERC20;
-    using SafeMath for uint256;
 
     // Tokens used
     address public native;
@@ -29,32 +24,28 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
     // Third party contracts
     address public rewardPool;
 
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
     // Routes
     address[] public outputToNativeRoute;
     address[] public outputToLp0Route;
     address[] public outputToLp1Route;
 
-    // View
-    string[] public outputToLp0SymbolRoute;
-    string[] public outputToLp1SymbolRoute;
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
 
-    /**
-     * @dev Event that is fired each time someone harvests the strat.
-     */
-    event StratHarvest(address indexed harvester);
-
-    constructor(
+    function initialize(
         address _want,
         address _rewardPool,
-        address _vault,
-        address _unirouter,
-        address _keeper,
-        address _strategist,
-        address _beefyFeeRecipient,
-        address[] memory _outputToNativeRoute,
-        address[] memory _outputToLp0Route,
-        address[] memory _outputToLp1Route
-    ) StratManager(_keeper, _strategist, _unirouter, _vault, _beefyFeeRecipient) public {
+        address[] calldata _outputToNativeRoute,
+        address[] calldata _outputToLp0Route,
+        address[] calldata _outputToLp1Route,
+        CommonAddresses calldata _commonAddresses
+    ) public initializer {
+        __StratFeeManager_init(_commonAddresses);
         want = _want;
         rewardPool = _rewardPool;
 
@@ -71,7 +62,6 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
 
         lpToken1 = IUniswapV2Pair(want).token1();
 
-
         _giveAllowances();
     }
 
@@ -81,6 +71,7 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
 
         if (wantBal > 0) {
             ISimpleStaking(rewardPool).deposit(wantBal);
+            emit Deposit(balanceOf());
         }
     }
 
@@ -90,7 +81,7 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
         uint256 wantBal = IERC20(want).balanceOf(address(this));
 
         if (wantBal < _amount) {
-            ISimpleStaking(rewardPool).withdraw(_amount.sub(wantBal));
+            ISimpleStaking(rewardPool).withdraw(_amount - wantBal);
             wantBal = IERC20(want).balanceOf(address(this));
         }
 
@@ -98,63 +89,100 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
             wantBal = _amount;
         }
 
-        if (tx.origin == owner() || paused()) {
-            IERC20(want).safeTransfer(vault, wantBal);
-        } else {
-            uint256 withdrawalFeeAmount = wantBal.mul(withdrawalFee).div(WITHDRAWAL_MAX);
-            IERC20(want).safeTransfer(vault, wantBal.sub(withdrawalFeeAmount));
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+
+        emit Withdraw(balanceOf());
+    }
+
+    function beforeDeposit() external virtual override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
         }
     }
 
+    function harvest() external gasThrottle virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external gasThrottle virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    function managerHarvest() external onlyManager {
+        _harvest(tx.origin);
+    }
+
     // compounds earnings and charges performance fee
-    function harvest() external whenNotPaused gasThrottle {
+    function _harvest(address callFeeRecipient) internal whenNotPaused {
         address[] memory users = new address[](1);
         users[0] = address(this);
         ISimpleStaking(rewardPool).claimReward(users);
-        chargeFees();
-        addLiquidity();
-        deposit();
+        uint256 outputBal = IERC20(output).balanceOf(address(this));
+        if (outputBal > 0) {
+            chargeFees(callFeeRecipient);
+            addLiquidity();
+            uint256 wantHarvested = balanceOfWant();
+            deposit();
 
-        emit StratHarvest(msg.sender);
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
     }
 
     // performance fees
-    function chargeFees() internal {
-        uint256 toNative = IERC20(output).balanceOf(address(this)).mul(45).div(1000);
-        IUniswapRouterETH(unirouter).swapExactTokensForTokens(toNative, 0, outputToNativeRoute, address(this), now);
+    function chargeFees(address callFeeRecipient) internal {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 toNative = IERC20(output).balanceOf(address(this)) * fees.total / DIVISOR;
+        IUniswapRouterETH(unirouter).swapExactTokensForTokens(
+            toNative, 0, outputToNativeRoute, address(this), block.timestamp
+        );
 
         uint256 nativeBal = IERC20(native).balanceOf(address(this));
 
-        uint256 callFeeAmount = nativeBal.mul(callFee).div(MAX_FEE);
-        IERC20(native).safeTransfer(tx.origin, callFeeAmount);
+        uint256 callFeeAmount = nativeBal * fees.call / DIVISOR;
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
 
-        uint256 beefyFeeAmount = nativeBal.mul(beefyFee).div(MAX_FEE);
+        uint256 beefyFeeAmount = nativeBal * fees.beefy / DIVISOR;
         IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
 
-        uint256 strategistFee = nativeBal.mul(STRATEGIST_FEE).div(MAX_FEE);
-        IERC20(native).safeTransfer(strategist, strategistFee);
+        uint256 strategistFeeAmount = nativeBal * fees.strategist / DIVISOR;
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
     }
 
     // Adds liquidity to AMM and gets more LP tokens.
     function addLiquidity() internal {
-        uint256 outputHalf = IERC20(output).balanceOf(address(this)).div(2);
+        uint256 outputHalf = IERC20(output).balanceOf(address(this)) / 2;
 
         if (lpToken0 != output) {
-            IUniswapRouterETH(unirouter).swapExactTokensForTokens(outputHalf, 0, outputToLp0Route, address(this), now);
+            IUniswapRouterETH(unirouter).swapExactTokensForTokens(
+                outputHalf, 0, outputToLp0Route, address(this), block.timestamp
+            );
         }
 
         if (lpToken1 != output) {
-            IUniswapRouterETH(unirouter).swapExactTokensForTokens(outputHalf, 0, outputToLp1Route, address(this), now);
+            IUniswapRouterETH(unirouter).swapExactTokensForTokens(
+                outputHalf, 0, outputToLp1Route, address(this), block.timestamp
+            );
         }
 
         uint256 lp0Bal = IERC20(lpToken0).balanceOf(address(this));
         uint256 lp1Bal = IERC20(lpToken1).balanceOf(address(this));
-        IUniswapRouterETH(unirouter).addLiquidity(lpToken0, lpToken1, lp0Bal, lp1Bal, 1, 1, address(this), now);
+        IUniswapRouterETH(unirouter).addLiquidity(
+            lpToken0, lpToken1, lp0Bal, lp1Bal, 1, 1, address(this), block.timestamp
+        );
     }
 
     // calculate the total underlaying 'want' held by the strat.
     function balanceOf() public view returns (uint256) {
-        return balanceOfWant().add(balanceOfPool());
+        return balanceOfWant() + balanceOfPool();
     }
 
     // it calculates how much 'want' this contract holds.
@@ -165,6 +193,36 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
     // it calculates how much 'want' the strategy has working in the farm.
     function balanceOfPool() public view returns (uint256) {
         return ISimpleStaking(rewardPool).userCollateral(address(this));
+    }
+
+    // returns rewards unharvested
+    function rewardsAvailable() public view returns (uint256) {
+        return ISimpleStaking(rewardPool).pendingReward(address(this));
+    }
+
+    // native reward amount for calling harvest
+    function callReward() external view returns (uint256) {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 outputBal = rewardsAvailable();
+        uint256 nativeOut;
+        if (outputBal > 0) {
+            uint256[] memory amountOut = IUniswapRouterETH(unirouter).getAmountsOut(
+                outputBal, outputToNativeRoute
+            );
+            nativeOut = amountOut[amountOut.length - 1];
+        }
+
+        return nativeOut * fees.total / DIVISOR * fees.call / DIVISOR;
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+
+        if (harvestOnDeposit) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
     }
 
     // called as part of strat migration. Sends all the available funds back to the vault.
@@ -197,22 +255,15 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
         deposit();
     }
 
-    function lp0AddressRoute() public view returns (address[] memory) {
-        return outputToLp0Route;
-    }
-    function lp1AddressRoute() public view returns (address[] memory) {
-        return outputToLp1Route;
-    }
-
     function _giveAllowances() internal {
-        IERC20(want).safeApprove(rewardPool, uint256(-1));
-        IERC20(output).safeApprove(unirouter, uint256(-1));
+        IERC20(want).safeApprove(rewardPool, type(uint).max);
+        IERC20(output).safeApprove(unirouter, type(uint).max);
 
         IERC20(lpToken0).safeApprove(unirouter, 0);
-        IERC20(lpToken0).safeApprove(unirouter, uint256(-1));
+        IERC20(lpToken0).safeApprove(unirouter, type(uint).max);
 
         IERC20(lpToken1).safeApprove(unirouter, 0);
-        IERC20(lpToken1).safeApprove(unirouter, uint256(-1));
+        IERC20(lpToken1).safeApprove(unirouter, type(uint).max);
     }
 
     function _removeAllowances() internal {
@@ -220,5 +271,17 @@ contract StrategyOpeanOceanRewardPoolLP is StratManager, FeeManager, GasThrottle
         IERC20(output).safeApprove(unirouter, 0);
         IERC20(lpToken0).safeApprove(unirouter, 0);
         IERC20(lpToken1).safeApprove(unirouter, 0);
+    }
+
+    function outputToNative() public view returns (address[] memory) {
+        return outputToNativeRoute;
+    }
+
+    function outputToLp0() public view returns (address[] memory) {
+        return outputToLp0Route;
+    }
+
+    function outputToLp1() public view returns (address[] memory) {
+        return outputToLp1Route;
     }
 }
