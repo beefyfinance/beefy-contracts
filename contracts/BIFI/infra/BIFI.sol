86f137c9e6c46fb936f3097d12cbaba10a127819
RM <roman-monk@users.noreply.github.com>
2023-08-14 16:17:53 +0700

    Add BIFI mint receiver

--- contracts/BIFI/infra/BIFI.sol
+++ contracts/BIFI/infra/BIFI.sol
@@ -6,8 +6,8 @@ import "@openzeppelin-4/contracts/token/ERC20/ERC20.sol";
 
 contract BIFI is ERC20 {
 
-    constructor() ERC20("Beefy", "BIFI")  {
-        _mint(msg.sender, 80_000 ether);
+    constructor(address treasury) ERC20("Beefy", "BIFI")  {
+        _mint(treasury, 80_000 ether);
     }
 
 }
