e3cbbd61ff302a11310c1238a4b14d51f4e452ca
EREN <eren@ERENs-Laptop.local>
2024-12-16 12:40:25 -0300

    remove multicall3 deployment

--- scripts/infra/deployAppMulticall.js
+++ scripts/infra/deployAppMulticall.js
@@ -7,18 +7,14 @@ async function main() {
 
   const MulticallV2 = await ethers.getContractFactory("BeefyV2AppMulticall");
   const Multicall = await ethers.getContractFactory("Multicall");
-  const Multicall3 = await ethers.getContractFactory("Multicall3");
 
   const multicallV2 = await MulticallV2.deploy();
   const multicall = await Multicall.deploy();
-  const multicall3 = await Multicall3.deploy();
 
   await multicallV2.deployed();
   await multicall.deployed();
-  await multicall3.deployed();
 
   console.log("Multicall deployed:", multicall.address);
-  console.log("Multicall3 deployed:", multicall3.address);
   console.log("App v2 multicall deployed:", multicallV2.address);
 }
 
