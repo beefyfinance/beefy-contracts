b7099671655293e906f4ae9d12dc7fe580ef37dc
Daniel_eth <danielvigo06@gmail.com>
2025-10-22 17:08:54 +0200

    increased gas efficiency in BeefyVaultV7Native

--- contracts/BIFI/vaults/BeefyVaultV7Native.sol
+++ contracts/BIFI/vaults/BeefyVaultV7Native.sol
@@ -26,9 +26,9 @@ contract BeefyVaultV7Native is ERC20Upgradeable, OwnableUpgradeable, ReentrancyG
     // The last proposed strategy to switch to.
     StratCandidate public stratCandidate;
     // The strategy currently in use by the vault.
-    IStrategyV7 public strategy;
+    IStrategyV7 public immutable strategy;
     // The minimum time it has to pass before a strat candidate can be approved.
-    uint256 public approvalDelay;
+    uint256 public immutable approvalDelay;
 
     event NewStratCandidate(address implementation);
     event UpgradeStrat(address implementation);
@@ -106,7 +106,7 @@ contract BeefyVaultV7Native is ERC20Upgradeable, OwnableUpgradeable, ReentrancyG
         earn();
         uint256 _after = balance();
         _amount = _after - _pool; // Additional check for deflationary tokens
-        uint256 shares = 0;
+        uint256 shares;
         if (totalSupply() == 0) {
             shares = _amount;
         } else {
@@ -128,7 +128,7 @@ contract BeefyVaultV7Native is ERC20Upgradeable, OwnableUpgradeable, ReentrancyG
         earn();
         uint256 _after = balance();
         _amount = _after - _pool; // Additional check for deflationary tokens
-        uint256 shares = 0;
+        uint256 shares;
         if (totalSupply() == 0) {
             shares = _amount;
         } else {
